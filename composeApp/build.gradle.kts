6ab8426f9378695e6412fee002b32037c5546cb0
Yiqun Zhang <guiyanakuang@gmail.com>
2023-11-17 11:30:41 +0800

    :tada: init project

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+    jvm("desktop")
+    
+    sourceSets {
+        val desktopMain by getting
+        
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.clipevery"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -0,0 +1,16 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
+
+@Preview
+@Composable
+fun AppDesktopPreview() {
+    App()
+}
