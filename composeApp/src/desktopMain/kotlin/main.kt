fe5e97a73f8c4f48c96655fb813f7af692a75182
Yiqun Zhang <guiyanakuang@gmail.com>
2023-11-19 15:20:36 +0800

    :sparkles: system tray support (#10)

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -2,13 +2,19 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.clipevery.ClipeveryApp
 import com.clipevery.getClipboard
+import org.jetbrains.skia.Image
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Transferable
+import java.io.InputStream
 import java.util.function.Consumer
+import kotlin.system.exitProcess
 
 
 fun main() = application {
@@ -25,9 +31,35 @@ fun main() = application {
     }
     val clipboard = getClipboard(consumer)
     clipboard.start()
-    Window(onCloseRequest = ::exitApplication) {
+
+    val imageBitmap = loadIconFromResources("clipevery_icon.png")
+
+
+    Tray(icon = imageBitmap,
+        menu = {
+            Item(
+                "Exit",
+                onClick = { exitProcess(1) }
+            )
+        }
+    )
+
+    Window(onCloseRequest = ::exitApplication,
+        title = "Clipevery",
+        icon = imageBitmap,
+        undecorated = true,
+        resizable = false) {
         ClipeveryApp(clipboard, copyText)
     }
+}
+
+fun loadIconFromResources(resourceName: String): BitmapPainter {
+    val resourceStream: InputStream? = Thread.currentThread().contextClassLoader.getResourceAsStream(resourceName)
+    resourceStream?.let {
+        val image = Image.makeFromEncoded(it.readAllBytes())
+        return BitmapPainter(image.toComposeImageBitmap())
+    }
+    throw IllegalArgumentException("Resource not found: $resourceName")
 }
 
 @Preview
