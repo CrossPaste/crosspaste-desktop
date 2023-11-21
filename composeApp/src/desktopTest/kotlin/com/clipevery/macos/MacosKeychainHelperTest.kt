0e7a19c6b7b329e707fdedef50f4c8b264b62788
Yiqun Zhang <guiyanakuang@gmail.com>
2023-11-21 15:32:33 +0800

    :sparkles: Create a tooltip for clipevery to interact with keychain

---
+++ composeApp/src/desktopTest/kotlin/com/clipevery/macos/MacosKeychainHelperTest.kt
@@ -0,0 +1,24 @@
+package com.clipevery.macos
+
+import com.clipevery.platform.currentPlatform
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class MacosKeychainHelperTest {
+
+    @Test
+    fun testKeychain() {
+        if (currentPlatform().isMacos()) {
+            MacosKeychainHelper.setPassword("com.clipevery", "test", "test")
+            var password = MacosKeychainHelper.getPassword("com.clipevery", "test")
+            assertEquals("test", password)
+            MacosKeychainHelper.updatePassword("com.clipevery", "test", "test1")
+            password = MacosKeychainHelper.getPassword("com.clipevery", "test")
+            assertEquals("test1", password)
+            assertTrue(MacosKeychainHelper.deletePassword("com.clipevery", "test"))
+            val password1 = MacosKeychainHelper.getPassword("com.clipevery", "test")
+            assertTrue(password1 == null)
+        }
+    }
+}
