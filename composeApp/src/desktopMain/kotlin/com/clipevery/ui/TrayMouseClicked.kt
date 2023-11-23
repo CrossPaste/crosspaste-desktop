9adda23bd9b4ac72bc8eedcc80a0c4a206e9629c
Yiqun Zhang <guiyanakuang@gmail.com>
2023-11-23 23:03:59 +0800

    :sparkles: Determines where the interface is displayed

--- composeApp/src/desktopMain/kotlin/com/clipevery/ui/TrayMouseClicked.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/ui/TrayMouseClicked.kt
@@ -6,14 +6,16 @@ import androidx.compose.ui.unit.times
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import com.clipevery.platform.currentPlatform
+import java.awt.GraphicsEnvironment
+import java.awt.Toolkit
 import java.awt.event.MouseAdapter
 
 
-fun getTrayMouseAdapter(windowState: WindowState, mouseClickedAction: () -> Unit): MouseAdapter {
+fun  getTrayMouseAdapter(windowState: WindowState, mouseClickedAction: () -> Unit): MouseAdapter {
     return if(currentPlatform().isMacos()) {
         MacTrayMouseClicked(windowState, mouseClickedAction)
     } else {
-        WindowsTrayMouseClicked()
+        WindowsTrayMouseClicked(windowState, mouseClickedAction)
     }
 }
 
@@ -38,9 +40,24 @@ class MacTrayMouseClicked(private val windowState: WindowState, private val mous
     }
 }
 
-class WindowsTrayMouseClicked: MouseAdapter() {
+class WindowsTrayMouseClicked(private val windowState: WindowState, private val mouseClickedAction: () -> Unit): MouseAdapter() {
 
     override fun mouseClicked(e: java.awt.event.MouseEvent) {
+        mouseClickedAction()
 
+        val gd = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice
+        val bounds = gd.defaultConfiguration.bounds
+        val insets = Toolkit.getDefaultToolkit().getScreenInsets(gd.defaultConfiguration)
+
+        val usableWidth = bounds.width - insets.right
+        val usableHeight = bounds.height - insets.bottom
+
+        val windowWidth = windowState.size.width
+        val windowHeight = windowState.size.height
+
+        windowState.position = WindowPosition.Absolute(
+            x = usableWidth.dp - windowWidth - 32.dp,
+            y = usableHeight.dp - windowHeight - 32.dp
+        )
     }
 }
