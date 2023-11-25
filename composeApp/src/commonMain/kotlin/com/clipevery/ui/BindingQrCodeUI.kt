c730a01f78b0d9bf628b8d7c8aae78c8e5af1744
Yiqun Zhang <guiyanakuang@gmail.com>
2023-11-25 17:20:53 +0800

    :lipstick: Create tabs page

--- composeApp/src/commonMain/kotlin/com/clipevery/ui/BindingQrCodeUI.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/BindingQrCodeUI.kt
@@ -1,101 +1,59 @@
-package com.clipevery
+package com.clipevery.ui
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.clipevery.ui.TabsUI
-import com.clipevery.ui.TitleUI
+import com.clipevery.LocalQRCodeGenerator
 import compose.icons.TablerIcons
 import compose.icons.tablericons.Scan
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
-fun ClipeveryApp(dependencies: Dependencies) {
-    MaterialTheme {
-        Column(Modifier.fillMaxWidth()
-            .clip(RoundedCornerShape(10.dp))
-            .background(Color(238, 238, 238)),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            ClipeveryCommon(dependencies)
+fun bindingQRCode() {
+    val qrCodeGenerator = LocalQRCodeGenerator.current
+    val coroutineScope = rememberCoroutineScope()
+    var refreshJob: Job? by remember { mutableStateOf(null) }
+
+    var qrImage by remember { mutableStateOf(qrCodeGenerator.generateQRCode(600, 600)) }
+
+    DisposableEffect(Unit) {
+        refreshJob = coroutineScope.launch {
+            while (true) {
+                qrImage = qrCodeGenerator.generateQRCode( 600, 600)
+                delay(5000)
+            }
+        }
+        onDispose {
+            refreshJob?.cancel()
         }
     }
-}
 
-
-@Composable
-fun ClipeveryCommon(dependencies: Dependencies) {
-    CompositionLocalProvider(
-        LocalClipeveryServer provides dependencies.clipServer,
-        LocalConfigManager provides dependencies.configManager,
-        LocalFilePersist provides dependencies.filePersist,
-        LocalSignalProtocol provides dependencies.signalProtocol,
-        LocalQRCodeGenerator provides dependencies.qrCodeGenerator,
-    ) {
-        ClipeveryWithProvidedDependencies()
-    }
-}
-
-@Composable
-fun ClipeveryWithProvidedDependencies() {
-    CustomWindowDecoration()
-    TabsUI()
-
-}
-
-@Composable
-fun CustomWindowDecoration() {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(62.dp),
-        color = MaterialTheme.colors.background,
-        shape = RoundedCornerShape(
-            topStart = 10.dp,
-            topEnd = 10.dp,
-            bottomEnd = 0.dp,
-            bottomStart = 0.dp
-        )
-    ) {
-        TitleUI()
-    }
-}
-
-@Composable
-fun mainUI() {
-    Text("mainUI")
-}
-
-
-@Preview
-@Composable
-fun ColumTest() {
     Column(
         modifier = Modifier.fillMaxSize().padding(10.dp),
         verticalArrangement = Arrangement.Center,
@@ -128,22 +86,15 @@ fun ColumTest() {
                     Icon(imageVector = TablerIcons.Scan,
                         contentDescription = "Scan",
                         modifier = Modifier.size(28.dp),
-                        tint = Color(red = 84, green = 135, blue = 237))
+                        tint = Color(red = 84, green = 135, blue = 237)
+                    )
                 }
             }
             Image(
-                bitmap = loadImageBitmap("clipevery_icon.png"),
+                bitmap = qrImage,
                 contentDescription = "QR Code",
                 modifier = Modifier.align(Alignment.Center)
             )
         }
     }
-}
-
-fun loadImageBitmap(resourcePath: String): ImageBitmap {
-    // Assuming `resourcePath` is a valid path for an image file within your resources directory.
-    val image = org.jetbrains.skia.Image.makeFromEncoded(
-        Thread.currentThread().contextClassLoader.getResourceAsStream(resourcePath)
-        ?.readBytes()!!)
-    return image.toComposeImageBitmap()
 }
---
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/TabsUI.kt
@@ -0,0 +1,139 @@
+package com.clipevery.ui
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Icon
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.clipevery.LocalConfigManager
+import com.clipevery.mainUI
+
+
+@Composable
+fun TabsUI() {
+    val configManager = LocalConfigManager.current
+    val config = remember { mutableStateOf(configManager.config) }
+    var selectedTabIndex by remember { mutableStateOf(0) }
+
+    val tabTitles = listOf("Clipboard", "Devices", "Scan")
+    Row(modifier = Modifier.padding(8.dp)
+        .wrapContentWidth()) {
+
+        tabTitles.forEachIndexed { index, title ->
+            Box(modifier = Modifier.padding(3.dp)
+                .wrapContentSize(Alignment.BottomStart)
+            )  {
+
+                val style = if (index == selectedTabIndex) {
+                    TextStyle(fontWeight = FontWeight.Bold)
+                } else {
+                    TextStyle(fontWeight = FontWeight.Normal)
+                }
+
+
+                Text(
+                    text = title,
+                    fontSize = 12.sp,
+                    style = style,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.BottomStart)
+                        .clickable { selectedTabIndex = index }
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.fillMaxWidth())
+    }
+
+    Column() {
+        when (selectedTabIndex) {
+            0 -> TabContent("Content for Tab 1")
+            1 -> TabContent("Content for Tab 2")
+            2 -> {
+                if (!config.value.bindingState) {
+                    bindingQRCode()
+                } else {
+                    mainUI()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun PreviewTabsUI() {
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    var searchText by remember { mutableStateOf("") }
+    val tabTitles = listOf("Tab 1", "Tab 2", "Tab 3")
+
+    Row(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+        Column(modifier = Modifier.padding(8.dp).wrapContentWidth().wrapContentHeight()) {
+            TabRow(
+                modifier = Modifier.wrapContentWidth().wrapContentHeight(),
+                selectedTabIndex = selectedTabIndex,
+                backgroundColor = Color.White
+            ) {
+                tabTitles.forEachIndexed { index, title ->
+                    Tab(
+                        modifier = Modifier.wrapContentWidth().wrapContentHeight(),
+                        selected = index == selectedTabIndex,
+                        onClick = { selectedTabIndex = index },
+                        text = { Text(modifier = Modifier.wrapContentWidth(), text = title, fontSize = 12.sp) }
+                    )
+                }
+            }
+        }
+        BasicTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            modifier = Modifier.width(100.dp), // Set a fixed width for the TextField
+            decorationBox = { innerTextField ->
+                Row {
+                    Icon(Icons.Filled.Search, contentDescription = "Search Icon")
+                    Box { innerTextField() }
+                }
+            }
+        )
+    }
+    Column() {
+        when (selectedTabIndex) {
+            0 -> TabContent("Content for Tab 1")
+            1 -> TabContent("Content for Tab 2")
+            2 -> TabContent("Content for Tab 3")
+        }
+    }
+}
+
+@Composable
+fun TabContent(text: String) {
+    Text(text = text)
+}
