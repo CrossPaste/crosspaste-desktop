c0bd3239e4f6ddb5bc1bbef25e794e6df39ab618
Yiqun Zhang <guiyanakuang@gmail.com>
2023-12-02 17:00:14 +0800

    :hammer: Refactoring the app info model

--- composeApp/src/desktopMain/kotlin/com/clipevery/device/DesktopDeviceInfoFactory.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/device/DesktopDeviceInfoFactory.kt
@@ -5,6 +5,10 @@ import com.clipevery.model.DeviceInfo
 import com.clipevery.model.HostInfo
 import com.clipevery.platform.Platform
 import com.clipevery.platform.currentPlatform
+import com.sun.jna.platform.win32.Kernel32Util
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 import java.net.NetworkInterface
 import java.util.Collections
 
@@ -14,7 +18,7 @@ class DesktopDeviceInfoFactory: DeviceInfoFactory {
         return if (platform.isMacos()) {
             getMacDeviceInfo(platform)
         } else if (platform.isWindows()) {
-            getWindowDeviceInfo()
+            getWindowDeviceInfo(platform)
         } else {
             throw IllegalStateException("Unsupported platform: $platform")
         }
@@ -32,8 +36,32 @@ private fun getMacDeviceInfo(platform: Platform): DeviceInfo {
     )
 }
 
-private fun getWindowDeviceInfo(): DeviceInfo {
-    TODO("Not yet implemented")
+private fun getWindowDeviceInfo(platform: Platform): DeviceInfo {
+    val deviceName = Kernel32Util.getComputerName()
+    val deviceId = getWindowDeviceId()
+    return DeviceInfo(
+        deviceId = deviceId,
+        deviceName = deviceName,
+        platform = platform,
+        hostInfoList = getHostInfoList()
+    )
+}
+
+fun getWindowDeviceId(): String {
+    try {
+        val command = "wmic csproduct get UUID"
+        val process = Runtime.getRuntime().exec(command)
+        val reader = BufferedReader(InputStreamReader(process.inputStream))
+        var line: String
+        while (reader.readLine().also { line = it } != null) {
+            if (line.trim { it <= ' ' }.isNotEmpty() && line.trim { it <= ' ' } != "UUID") {
+                return line.trim { it <= ' ' }
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return "Unknown"
 }
 
 
