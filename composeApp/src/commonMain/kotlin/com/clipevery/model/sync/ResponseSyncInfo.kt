51a5e1b2c198edba28562f053a8b6ab7df80e982
Yiqun Zhang <guiyanakuang@gmail.com>
2023-12-18 15:46:43 +0800

    :bug: Fix ResponseSyncInfo not marked as serializable

--- composeApp/src/commonMain/kotlin/com/clipevery/model/sync/ResponseSyncInfo.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/model/sync/ResponseSyncInfo.kt
@@ -2,7 +2,9 @@ package com.clipevery.model.sync
 
 import com.clipevery.app.AppInfo
 import com.clipevery.endpoint.EndpointInfo
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ResponseSyncInfo(val appInfo: AppInfo,
                             val endpointInfo: EndpointInfo
 )
