ff346f2c758932559a6ca5edcb88982486eed83c
Yiqun Zhang <guiyanakuang@gmail.com>
2023-12-26 14:17:31 +0800

    :sparkles: Impl an encrypted post get request for the client

--- composeApp/src/desktopMain/kotlin/com/clipevery/net/DesktopClipClient.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/net/DesktopClipClient.kt
@@ -2,9 +2,45 @@ package com.clipevery.net
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.InternalAPI
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToStream
+import org.signal.libsignal.protocol.SessionCipher
+import java.io.ByteArrayOutputStream
 
 class DesktopClipClient: ClipClient {
 
-    val client = HttpClient(CIO)
+    val client: HttpClient = HttpClient(CIO)
 
+
+    @OptIn(InternalAPI::class, ExperimentalSerializationApi::class)
+    suspend inline fun <reified T : Any> post(url: String,
+                                              appInstanceId: String,
+                                              sessionCipher: SessionCipher,
+                                              message: T): HttpResponse {
+        return client.post(url) {
+            header("appInstanceId", appInstanceId)
+            val outputStream = ByteArrayOutputStream()
+            Json.encodeToStream(message, outputStream)
+            body = sessionCipher.encrypt(outputStream.toByteArray())
+        }
+    }
+
+    @OptIn(InternalAPI::class, ExperimentalSerializationApi::class)
+    suspend inline fun <reified T : Any> get(url: String,
+                                             appInstanceId: String,
+                                             sessionCipher: SessionCipher,
+                                             message: T): HttpResponse {
+        return client.get(url) {
+            header("appInstanceId", appInstanceId)
+            val outputStream = ByteArrayOutputStream()
+            Json.encodeToStream(message, outputStream)
+            body = sessionCipher.encrypt(outputStream.toByteArray())
+        }
+    }
 }
