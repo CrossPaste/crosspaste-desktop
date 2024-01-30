cc3b5453e0c7abad4702c2804eed0e63c96407fd
Yiqun Zhang <guiyanakuang@gmail.com>
2024-01-30 11:23:29 +0800

    :sparkles: Add a menu button to the device list

--- composeApp/src/commonMain/kotlin/com/clipevery/ui/devices/DeviceBarView.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/devices/DeviceBarView.kt
@@ -3,6 +3,7 @@ package com.clipevery.ui.devices
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,13 +12,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -27,23 +32,34 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import com.clipevery.LocalKoinApplication
 import com.clipevery.dao.sync.SyncRuntimeInfo
 import com.clipevery.dao.sync.SyncState
 import com.clipevery.i18n.GlobalCopywriter
 import com.clipevery.platform.Platform
+import com.clipevery.ui.MenuItem
 import com.clipevery.ui.PageViewContext
 import com.clipevery.ui.PageViewType
 import com.clipevery.ui.android
+import com.clipevery.ui.base.ClipIconButton
 import com.clipevery.ui.icon.arrowLeftIcon
 import com.clipevery.ui.icon.arrowRightIcon
 import com.clipevery.ui.icon.block
@@ -65,6 +81,8 @@ fun DeviceBarView(syncRuntimeInfo: SyncRuntimeInfo,
 
     var hover by remember { mutableStateOf(false) }
     val backgroundColor = if (hover) MaterialTheme.colors.secondaryVariant else MaterialTheme.colors.background
+
+    val density = LocalDensity.current
 
     val (connectColor, connectText) = if (syncRuntimeInfo.allowSend || syncRuntimeInfo.allowReceive)  {
         getConnectStateColorAndText(syncRuntimeInfo.connectState)
@@ -185,12 +203,73 @@ fun DeviceBarView(syncRuntimeInfo: SyncRuntimeInfo,
             )
         )
 
+        var showPopup by remember { mutableStateOf(false) }
+
+        var buttonPosition by remember { mutableStateOf(Offset.Zero) }
+        var buttonSize by remember { mutableStateOf(androidx.compose.ui.geometry.Size(0.0f, 0.0f)) }
+
         if (deviceInteractionEnabled) {
-            Icon(
-                Icons.Outlined.Info,
-                contentDescription = "info",
-                modifier = Modifier.padding(16.dp).size(20.dp),
-                tint = MaterialTheme.colors.primary)
+            ClipIconButton(
+                radius = 18.dp,
+                onClick = {
+                    showPopup = !showPopup
+                },
+                modifier = Modifier
+                    .background(Color.Transparent, CircleShape) // Set the background to blue and shape to circle
+                    .onGloballyPositioned { coordinates ->
+                        buttonPosition = coordinates.localToWindow(Offset.Zero)
+                        buttonSize = coordinates.size.toSize()
+                    }
+
+            ) {
+                Icon(
+                    Icons.Outlined.MoreVert,
+                    contentDescription = "info",
+                    modifier = Modifier.padding(3.dp).size(18.dp),
+                    tint = MaterialTheme.colors.primary
+                )
+            }
+
+            if (showPopup) {
+                Popup(
+                    alignment = Alignment.TopEnd,
+                    offset = IntOffset(
+                        with(density) { ((-14).dp).roundToPx() },
+                        with(density) { (50.dp).roundToPx() },
+                    ),
+                    onDismissRequest = {
+                        if (showPopup) {
+                            showPopup = false
+                        }
+                    },
+                    properties = PopupProperties(
+                        focusable = true,
+                        dismissOnBackPress = true,
+                        dismissOnClickOutside = true
+
+                    )
+                ) {
+                    Box(modifier = Modifier
+                        .wrapContentSize()
+                        .background(Color.Transparent)
+                        .shadow(15.dp)) {
+                        Column(
+                            modifier = Modifier
+                                .width(180.dp)
+                                .wrapContentHeight()
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(MaterialTheme.colors.surface)
+                        ) {
+                            MenuItem(copywriter.getText("Remove_Device")) {
+                                // TODO: remove device
+                                showPopup = false
+                            }
+                        }
+                    }
+                }
+            }
+
+
         } else {
             Spacer(modifier = Modifier.width(16.dp))
         }
