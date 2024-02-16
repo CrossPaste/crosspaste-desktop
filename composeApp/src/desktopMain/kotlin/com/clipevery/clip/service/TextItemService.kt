3c53ffd5199331b5a02fb464a8aa3fd47ba40fe0
Yiqun Zhang <guiyanakuang@gmail.com>
2024-02-16 22:46:19 +0800

    :sparkles: Implement collection of text pasted items

--- composeApp/src/desktopMain/kotlin/com/clipevery/clip/service/TextItemService.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/clip/service/TextItemService.kt
@@ -2,12 +2,23 @@ package com.clipevery.clip.service
 
 import com.clipevery.clip.ClipCollector
 import com.clipevery.clip.ClipItemService
+import com.clipevery.clip.item.TextClipItem
+import com.clipevery.dao.clip.ClipAppearItem
+import com.clipevery.utils.md5ByString
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Transferable
 
 class TextItemService: ClipItemService {
+
+    companion object TextItemService {
+
+        const val UNICODE_STRING = "Unicode String"
+        const val TEXT = "text/plain"
+        const val PLAIN_TEXT = "Plain Text"
+    }
+
     override fun getIdentifiers(): List<String> {
-        return listOf()
+        return listOf(UNICODE_STRING, TEXT, PLAIN_TEXT)
     }
 
     override fun doCreateClipItem(
@@ -18,6 +29,14 @@ class TextItemService: ClipItemService {
         transferable: Transferable,
         clipCollector: ClipCollector
     ) {
-        TODO("Not yet implemented")
+        var clipItem: ClipAppearItem? = null
+        if (transferData is String) {
+            clipItem = TextClipItem().apply {
+                identifier = dataFlavor.humanPresentableName
+                text = transferData
+                md5 = md5ByString(text)
+            }
+        }
+        clipItem?.let { clipCollector.collectItem(itemIndex, this::class, it) }
     }
 }
