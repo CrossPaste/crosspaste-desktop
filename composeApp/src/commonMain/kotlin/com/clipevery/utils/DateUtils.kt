37975dc7217fb9722bece70623943cfa9b51e8ce
Yiqun Zhang <guiyanakuang@gmail.com>
2024-02-18 15:54:44 +0800

    :bug: Fix realm RealmInstant type filtering

--- composeApp/src/commonMain/kotlin/com/clipevery/utils/DateUtils.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/utils/DateUtils.kt
@@ -1,12 +1,16 @@
 package com.clipevery.utils
 
+import io.realm.kotlin.types.RealmInstant
 import java.util.Calendar
 
 object DateUtils {
 
-    fun getPrevDay(): Long {
+    fun getPrevDay(): RealmInstant {
         val calendar = Calendar.getInstance()
         calendar.add(Calendar.DAY_OF_MONTH, -1)
-        return calendar.timeInMillis
+        // Convert milliseconds to seconds for the epochSeconds parameter
+        val epochSeconds = calendar.timeInMillis / 1000
+        // The nanosecondAdjustment parameter is 0 as we're not adjusting nanoseconds here
+        return RealmInstant.from(epochSeconds, 0)
     }
 }
