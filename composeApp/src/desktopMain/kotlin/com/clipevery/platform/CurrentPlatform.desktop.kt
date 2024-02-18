299c6cfb393e58a41e2d97653480c652ce07a795
Yiqun Zhang <guiyanakuang@gmail.com>
2024-02-18 12:09:55 +0800

    :bug: Fix the issue of not correctly obtaining the Windows 11 version

--- composeApp/src/desktopMain/kotlin/com/clipevery/platform/CurrentPlatform.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/platform/CurrentPlatform.desktop.kt
@@ -1,6 +1,8 @@
 package com.clipevery.platform
 
 import com.clipevery.utils.OnceFunction
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 actual fun currentPlatform(): Platform {
     return OnceFunction { getCurrentPlatform() }.run()
@@ -16,9 +18,25 @@ private fun getCurrentPlatform(): Platform {
         32
     }
     return when {
-        "win" in osName -> Platform(name = "Windows", arch = architecture, bitMode = bitMode, version = version)
+        "win" in osName -> Platform(name = "Windows", arch = architecture, bitMode = bitMode, version = getWindowsVersion())
         "mac" in osName -> Platform(name = "Macos", arch = architecture, bitMode = bitMode, version = version)
         "nix" in osName || "nux" in osName || "aix" in osName -> Platform(name = "Linux", arch = architecture, bitMode = bitMode, version = version)
         else -> Platform(name = "Unknown", arch = architecture, bitMode = bitMode, version = version)
     }
+}
+
+private fun getWindowsVersion(): String {
+    try {
+        val process = Runtime.getRuntime().exec("reg query \"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\" /v DisplayVersion")
+        val reader = BufferedReader(InputStreamReader(process.inputStream))
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            if (line!!.contains("DisplayVersion")) {
+                return line!!.split(" ").last()
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return "Unknown"
 }
