840e33170523c2b52a784bd02023f006117dddeb
Yiqun Zhang <guiyanakuang@gmail.com>
2024-02-29 12:24:55 +0800

    :bug: Fix the problem that the window size does not handle the scaling ratio when using html2image on the win side

--- composeApp/src/desktopMain/kotlin/com/clipevery/os/windows/WindowDpiHelper.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/os/windows/WindowDpiHelper.kt
@@ -2,12 +2,12 @@ package com.clipevery.os.windows
 
 import com.clipevery.os.windows.api.User32
 import com.sun.jna.Native
-import com.sun.jna.Pointer
 import com.sun.jna.platform.win32.WinDef
 import com.sun.jna.platform.win32.WinDef.LPARAM
 import com.sun.jna.platform.win32.WinDef.RECT
 import com.sun.jna.platform.win32.WinUser.HMONITOR
 import com.sun.jna.platform.win32.WinUser.MONITORENUMPROC
+import com.sun.jna.ptr.IntByReference
 import com.sun.jna.win32.StdCallLibrary
 import kotlin.math.max
 
@@ -15,7 +15,7 @@ import kotlin.math.max
 object WindowDpiHelper {
 
     interface Shcore : StdCallLibrary {
-        fun GetDpiForMonitor(hMonitor: HMONITOR?, dpiType: Int, dpiX: Pointer?, dpiY: Pointer?): Int
+        fun GetDpiForMonitor(hMonitor: HMONITOR?, dpiType: Int, dpiX: IntByReference, dpiY: IntByReference): Int
 
         companion object {
             val INSTANCE: Shcore = Native.load("shcore", Shcore::class.java)
@@ -23,16 +23,15 @@ object WindowDpiHelper {
     }
 
     fun getMaxDpiForMonitor(): Int {
-        var maxDpi = 0;
+        var maxDpi = 0
         val monitorEnumProc: MONITORENUMPROC = object : MONITORENUMPROC {
             override fun apply(hMonitor: HMONITOR?, hdcMonitor: WinDef.HDC?, lprcMonitor: RECT?, dwData: LPARAM?): Int {
-                val dpiX = Pointer(0)
-                val dpiY = Pointer(0)
+                val dpiX = IntByReference()
+                val dpiY = IntByReference()
 
                 Shcore.INSTANCE.GetDpiForMonitor(hMonitor, 0,  /* MDT_EFFECTIVE_DPI */dpiX, dpiY)
-                val currentDpiX = dpiX.getInt(0)
-                val currentDpiY = dpiY.getInt(0)
-
+                val currentDpiX = dpiX.getValue()
+                val currentDpiY = dpiY.getValue()
                 maxDpi = Math.max(maxDpi, max(currentDpiX, currentDpiY))
 
                 return 1 // continue enumeration
