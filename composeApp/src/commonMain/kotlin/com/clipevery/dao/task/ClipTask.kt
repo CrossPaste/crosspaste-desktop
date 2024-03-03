12a7193aeeff86fc780990bc67af1c3e065c2621
Yiqun Zhang <guiyanakuang@gmail.com>
2024-03-03 18:16:49 +0800

    :sparkles: Implement Task database design

---
+++ composeApp/src/commonMain/kotlin/com/clipevery/dao/task/ClipTask.kt
@@ -0,0 +1,30 @@
+package com.clipevery.dao.task
+
+import io.realm.kotlin.types.RealmAny
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.Index
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class ClipTask: RealmObject {
+    @PrimaryKey
+    var taskId: ObjectId = ObjectId()
+
+    @Index
+    var clipId: Int = 0
+
+    @Index
+    var taskType: Int = TaskType.UNKNOWN_TASK
+
+    @Index
+    var status: Int = 0
+
+    @Index
+    var createTime: RealmInstant = RealmInstant.now()
+
+    @Index
+    var modifyTime: RealmInstant = RealmInstant.now()
+
+    var extraInfo: RealmAny? = null
+}
---
+++ composeApp/src/commonMain/kotlin/com/clipevery/dao/task/TaskStatus.kt
@@ -0,0 +1,8 @@
+package com.clipevery.dao.task
+
+object TaskStatus {
+    const val PREPARING = 0
+    const val EXECUTING = 1
+    const val SUCCESS = 2
+    const val FAILURE = 3
+}
