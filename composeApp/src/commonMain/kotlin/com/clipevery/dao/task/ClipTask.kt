ee913f76a16ae3fb37f3fc8280e35a3621f9b982
Yiqun Zhang <guiyanakuang@gmail.com>
2024-03-10 10:57:04 +0800

    :fire: Remove invalid imports (#514)

--- composeApp/src/commonMain/kotlin/com/clipevery/dao/task/ClipTask.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/dao/task/ClipTask.kt
@@ -1,6 +1,5 @@
 package com.clipevery.dao.task
 
-import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.Index
 import io.realm.kotlin.types.annotations.PrimaryKey
