b59b18a063cff880f27ca27aba2b97bf4a2fd958
Yiqun Zhang <guiyanakuang@gmail.com>
2024-03-11 18:50:55 +0800

    :sparkles: Implement build transferable from clipData (#532)

--- composeApp/src/desktopMain/kotlin/com/clipevery/clip/DesktopTransferableProducer.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/clip/DesktopTransferableProducer.kt
@@ -1,31 +1,73 @@
 package com.clipevery.clip
 
+import com.clipevery.clip.item.FilesClipItem
+import com.clipevery.clip.item.HtmlClipItem
+import com.clipevery.clip.item.ImagesClipItem
+import com.clipevery.clip.item.TextClipItem
+import com.clipevery.clip.item.UrlClipItem
 import com.clipevery.dao.clip.ClipData
+import java.awt.Image
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.Transferable
+import java.io.File
+import java.net.URL
+import javax.imageio.ImageIO
 
 class DesktopTransferableProducer: TransferableProducer {
     override fun produce(clipData: ClipData): Transferable {
-        val dataFlavors = buildDataFlavorArray(clipData)
-        return object: Transferable {
-            override fun getTransferDataFlavors(): Array<DataFlavor> {
-                return dataFlavors
-            }
+        val map = LinkedHashMap<DataFlavor, Any>()
 
-            override fun isDataFlavorSupported(flavor: DataFlavor?): Boolean {
-                return dataFlavors.contains(flavor)
-            }
+        val clipAppearItems = clipData.getClipAppearItems()
 
-            override fun getTransferData(flavor: DataFlavor?): Any {
-                return flavor?.let {
-                    (it as? ClipDataFlavor)?.getData()
-                } ?: ""
+        for (clipAppearItem in clipAppearItems) {
+            when(clipAppearItem) {
+                is FilesClipItem -> {
+                    addDataFlavor(map, clipAppearItem)
+                }
+                is HtmlClipItem -> {
+                    addDataFlavor(map, clipAppearItem)
+                }
+                is ImagesClipItem -> {
+                    addDataFlavor(map, clipAppearItem)
+                }
+                is TextClipItem -> {
+                    addDataFlavor(map, clipAppearItem)
+                }
+                is UrlClipItem -> {
+                    addDataFlavor(map, clipAppearItem)
+                }
             }
+        }
+        return ClipTransferable(map)
+    }
+
+    private fun addDataFlavor(map: MutableMap<DataFlavor, Any>, filesClipItem: FilesClipItem) {
+        val fileList: List<File> = filesClipItem.getFilePaths().map { it.toFile() }
+        map[DataFlavor.javaFileListFlavor] = fileList
+    }
+
+    private fun addDataFlavor(map: MutableMap<DataFlavor, Any>, htmlClipItem: HtmlClipItem) {
+        map[DataFlavor.selectionHtmlFlavor] = htmlClipItem.html
+    }
+
+    private fun addDataFlavor(map: MutableMap<DataFlavor, Any>, imagesClipItem: ImagesClipItem) {
+        val filePaths = imagesClipItem.getFilePaths()
+        if (filePaths.size == 1) {
+            val imageFile = filePaths[0].toFile()
+            val bufferedImage: Image = ImageIO.read(imageFile)
+            map[DataFlavor.imageFlavor] = bufferedImage
+            map[DataFlavor.javaFileListFlavor] = listOf(imageFile)
+        } else {
+            val fileList: List<File> = imagesClipItem.getFilePaths().map { it.toFile() }
+            map[DataFlavor.javaFileListFlavor] = fileList
         }
     }
 
-    private fun buildDataFlavorArray(clipData: ClipData): Array<DataFlavor> {
-        // todo implement build data flavor array from clipData
-        return Array(0) { DataFlavor() }
+    private fun addDataFlavor(map: MutableMap<DataFlavor, Any>, textClipItem: TextClipItem) {
+        map[DataFlavor.stringFlavor] = textClipItem.text
+    }
+
+    private fun addDataFlavor(map: MutableMap<DataFlavor, Any>, urlClipItem: UrlClipItem) {
+        map[DataFlavor("application/x-java-url; class=java.net.URL")] = URL(urlClipItem.url)
     }
 }
