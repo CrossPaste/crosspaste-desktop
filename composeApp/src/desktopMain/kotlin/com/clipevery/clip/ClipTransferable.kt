91e0a3e3e599e99cf86f3cbecd56729edf6c90a6
Yiqun Zhang <guiyanakuang@gmail.com>
2024-03-11 18:46:57 +0800

    :sparkles: Implement build transferable from clipData

---
+++ composeApp/src/desktopMain/kotlin/com/clipevery/clip/ClipTransferable.kt
@@ -0,0 +1,21 @@
+package com.clipevery.clip
+
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.Transferable
+
+class ClipTransferable(private val map: LinkedHashMap<DataFlavor, Any>): Transferable {
+
+    private val dataFlavors = map.keys.toTypedArray()
+
+    override fun getTransferDataFlavors(): Array<DataFlavor> {
+        return dataFlavors
+    }
+
+    override fun isDataFlavorSupported(flavor: DataFlavor?): Boolean {
+        return map.containsKey(flavor)
+    }
+
+    override fun getTransferData(flavor: DataFlavor?): Any {
+        return map[flavor] ?: ""
+    }
+}
