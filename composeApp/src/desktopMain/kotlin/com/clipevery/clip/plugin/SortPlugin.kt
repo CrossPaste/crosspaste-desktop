4c6dde82925df180cc154facfdd95f78daa48a93
Yiqun Zhang <guiyanakuang@gmail.com>
2024-03-31 13:07:13 +0800

    :sparkles: Supports extensible pasted item type sorting

--- composeApp/src/desktopMain/kotlin/com/clipevery/clip/plugin/SortPlugin.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/clip/plugin/SortPlugin.kt
@@ -2,10 +2,21 @@ package com.clipevery.clip.plugin
 
 import com.clipevery.clip.ClipPlugin
 import com.clipevery.dao.clip.ClipAppearItem
+import com.clipevery.dao.clip.ClipType
 import io.realm.kotlin.MutableRealm
 
 object SortPlugin: ClipPlugin {
+
+    private val itemPriorityMap: Map<Int, Int> = mapOf(
+        ClipType.IMAGE to 4,
+        ClipType.FILE to 3,
+        ClipType.HTML to 2,
+        ClipType.URL to 1,
+        ClipType.TEXT to 0,
+        ClipType.INVALID to -1
+    )
+
     override fun pluginProcess(clipAppearItems: List<ClipAppearItem>, realm: MutableRealm): List<ClipAppearItem> {
-        return clipAppearItems.sortedByDescending { it.getClipType() }
+        return clipAppearItems.sortedByDescending { itemPriorityMap[it.getClipType()]!! }
     }
 }
