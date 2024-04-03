6f86aa9c67d66debc2944175552a7f685d550efa
Yiqun Zhang <guiyanakuang@gmail.com>
2024-04-03 22:03:01 +0800

    :lipstick: Use thumbnail to display images to avoid lags

--- composeApp/src/commonMain/kotlin/com/clipevery/ui/clip/SingleImagePreviewView.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/clip/SingleImagePreviewView.kt
@@ -13,42 +13,45 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.clipevery.LocalKoinApplication
+import com.clipevery.clip.CacheManager
 import com.clipevery.i18n.GlobalCopywriter
-import com.clipevery.ui.base.imageSlash
-import com.clipevery.ui.resource.ClipResourceLoader
+import com.clipevery.ui.base.createThumbnail
+import com.clipevery.ui.base.getThumbnailPath
+import com.clipevery.ui.base.image
 import com.clipevery.utils.FileUtils
 import java.awt.Desktop
 import java.nio.file.Path
-import kotlin.io.path.absolutePathString
+import kotlin.io.path.exists
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SingleImagePreviewView(imagePath: Path) {
     val current = LocalKoinApplication.current
     val copywriter = current.koin.get<GlobalCopywriter>()
     val fileUtils = current.koin.get<FileUtils>()
-    val fileResourceLoader = current.koin.get<ClipResourceLoader>()
 
-    val existFile by remember { mutableStateOf(imagePath.toFile().exists()) }
+    val existFile by remember { mutableStateOf(imagePath.exists()) }
 
-    val painter = if (existFile) {
-        painterResource(imagePath.absolutePathString(), fileResourceLoader)
-    } else {
-        imageSlash()
+    var existThumbnailFile by remember { mutableStateOf(getThumbnailPath(imagePath).exists()) }
+
+    LaunchedEffect(Unit) {
+        if (existFile && !existThumbnailFile) {
+            createThumbnail(imagePath)
+            existThumbnailFile = getThumbnailPath(imagePath).exists()
+        }
     }
 
     Row(modifier = Modifier.clickable {
@@ -57,13 +60,7 @@ fun SingleImagePreviewView(imagePath: Path) {
             desktop.open(imagePath.toFile())
         }
     }) {
-        Image(
-            modifier = Modifier.size(100.dp)
-                .clip(RoundedCornerShape(5.dp)),
-            painter = painter,
-            contentDescription = "Image",
-            contentScale = ContentScale.Crop
-        )
+        ThumbnailImage(imagePath, existThumbnailFile)
 
         Column(
             modifier = Modifier.fillMaxHeight()
@@ -80,16 +77,6 @@ fun SingleImagePreviewView(imagePath: Path) {
                 )
             )
             if (existFile) {
-                Text(
-                    text = "${copywriter.getText("Dimensions")}: " +
-                            "${painter.intrinsicSize.width} x ${painter.intrinsicSize.height}",
-                    color = MaterialTheme.colors.onBackground,
-                    style = TextStyle(
-                        fontWeight = FontWeight.Light,
-                        fontSize = 10.sp
-                    )
-                )
-
                 Text(
                     text = "${copywriter.getText("Size")}: ${fileUtils.getFileSize(imagePath)}",
                     color = MaterialTheme.colors.onBackground,
@@ -110,5 +97,33 @@ fun SingleImagePreviewView(imagePath: Path) {
                 )
             }
         }
+    }
+}
+
+
+@Composable
+fun ThumbnailImage(imagePath: Path, existThumbnailFile: Boolean) {
+    val current = LocalKoinApplication.current
+    val imagesCaches = current.koin.get<CacheManager>().imagesCaches
+
+    if (existThumbnailFile) {
+        val imageBitmap = remember(imagePath) {
+            imagesCaches.get(getThumbnailPath(imagePath))
+        }
+        Image(
+            modifier = Modifier.size(100.dp)
+                .clip(RoundedCornerShape(5.dp)),
+            bitmap = imageBitmap,
+            contentDescription = "Image",
+            contentScale = ContentScale.Crop
+        )
+    } else {
+        Image(
+            modifier = Modifier.size(100.dp)
+                .clip(RoundedCornerShape(5.dp)),
+            painter = image(),
+            contentDescription = "Image",
+            contentScale = ContentScale.Crop
+        )
     }
 }
--- composeApp/src/desktopMain/kotlin/com/clipevery/routing/PullRouting.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/routing/PullRouting.kt
@@ -3,18 +3,22 @@ package com.clipevery.routing
 import com.clipevery.Dependencies
 import com.clipevery.clip.CacheManager
 import com.clipevery.dto.pull.PullFileRequest
+import com.clipevery.dto.pull.PullFilesKey
 import com.clipevery.exception.StandardErrorCode
 import com.clipevery.sync.SyncManager
 import com.clipevery.utils.FileUtils
 import com.clipevery.utils.failResponse
 import com.clipevery.utils.getAppInstanceId
 import com.clipevery.utils.successResponse
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import io.ktor.utils.io.*
 
 fun Routing.pullRouting() {
+
+    val logger = KotlinLogging.logger {}
 
     val koinApplication = Dependencies.koinApplication
 
@@ -32,10 +36,12 @@ fun Routing.pullRouting() {
 
             syncManager.getSyncHandlers()[fromAppInstanceId]?.let {
                 if (!it.syncRuntimeInfo.allowSend) {
+                    logger.debug { "sync handler ($fromAppInstanceId) not allow send" }
                     failResponse(call, StandardErrorCode.SYNC_NOT_ALLOW_SEND.toErrorCode())
                     return@post
                 }
             } ?: run {
+                logger.error { "not found appInstance id: $fromAppInstanceId" }
                 failResponse(call, StandardErrorCode.NOT_FOUND_APP_INSTANCE_ID.toErrorCode())
                 return@post
             }
@@ -46,11 +52,12 @@ fun Routing.pullRouting() {
                         fileUtils.readFilesChunk(chunk, this)
                     }
                     successResponse(call, producer)
-                } ?: failResponse(call, StandardErrorCode.OUT_RANGE_CHUNK_INDEX.toErrorCode(), "out range chunk index ${pullFileRequest.chunkIndex}")
+                } ?: run {
+                    logger.error { "$fromAppInstanceId get $appInstanceId chunk out range: ${pullFileRequest.chunkIndex}" }
+                    failResponse(call, StandardErrorCode.OUT_RANGE_CHUNK_INDEX.toErrorCode(),
+                        "out range chunk index ${pullFileRequest.chunkIndex}")
+                }
             }
         }
     }
 }
-
-data class PullFilesKey(val appInstanceId: String,
-                        val clipId: Long)
