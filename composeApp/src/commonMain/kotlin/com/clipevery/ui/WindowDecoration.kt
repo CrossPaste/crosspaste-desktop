0bda5a4735f860b9d97f3d7e8208bf2040c03cce
Yiqun Zhang <guiyanakuang@gmail.com>
2024-04-06 22:28:21 +0800

    :lipstick: Adjust window decoration (#678)

--- composeApp/src/commonMain/kotlin/com/clipevery/ui/WindowDecoration.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/WindowDecoration.kt
@@ -2,9 +2,10 @@ package com.clipevery.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,7 +19,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -26,7 +26,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.clipevery.LocalKoinApplication
@@ -56,18 +55,6 @@ fun DecorationUI(currentPageViewContext: MutableState<PageViewContext>, title: S
     val current = LocalKoinApplication.current
     val copywriter = current.koin.get<GlobalCopywriter>()
 
-    val customFontFamily = remember(copywriter.language()) {
-        if (copywriter.language() == "zh") {
-            FontFamily(
-                Font(resource = "font/SourceHanSansCN-Bold.otf", FontWeight.Normal)
-            )
-        } else {
-            FontFamily(
-                Font(resource = "font/BebasNeue.otf", FontWeight.Normal)
-            )
-        }
-    }
-
     Box(
         modifier = Modifier
             .background(Color(0xFF121314))
@@ -82,39 +69,43 @@ fun DecorationUI(currentPageViewContext: MutableState<PageViewContext>, title: S
                 )
             ),
     ) {
-        Row(modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically) {
-            Row(modifier = Modifier.wrapContentSize()
-                .padding(start = 20.dp)
-                .clickable { currentPageViewContext.value = currentPageViewContext.value.returnNext() },
+
+        Column(modifier = Modifier.wrapContentSize()
+            .padding(horizontal = 20.dp)) {
+            Row(modifier = Modifier.fillMaxSize(),
                 verticalAlignment = Alignment.CenterVertically) {
-                Icon(
-                    painter = arrowBack(),
-                    contentDescription = "return",
-                    modifier = Modifier.size(20.dp),
-                    tint = MaterialTheme.colors.primary
-                )
+                Row(modifier = Modifier.wrapContentSize()
+                    .clickable { currentPageViewContext.value = currentPageViewContext.value.returnNext() },
+                    verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = arrowBack(),
+                        contentDescription = "return",
+                        modifier = Modifier.size(20.dp),
+                        tint = MaterialTheme.colors.primary
+                    )
+
+                    Text(
+                        text = copywriter.getText("Return"),
+                        style = TextStyle(
+                            fontWeight = FontWeight.Light,
+                            color = MaterialTheme.colors.primary,
+                            fontFamily = FontFamily.SansSerif,
+                            fontSize = 22.sp
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
 
                 Text(
-                    text = copywriter.getText("Return"),
-                    style = TextStyle(
-                        fontWeight = FontWeight.Light,
-                        color = MaterialTheme.colors.primary,
-                        fontFamily = customFontFamily,
-                        fontSize = 22.sp
-                    )
+                    modifier = Modifier,
+                    text = copywriter.getText(title),
+                    color = Color.White,
+                    fontSize = 22.sp,
+                    style = TextStyle(fontWeight = FontWeight.Bold),
+                    fontFamily = FontFamily.SansSerif
                 )
             }
-
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = copywriter.getText(title),
-                color = Color.White,
-                fontSize = 22.sp,
-                style = TextStyle(fontWeight = FontWeight.Bold),
-                fontFamily = customFontFamily
-            )
         }
     }
 }
