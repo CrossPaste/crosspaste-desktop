be36e82c5464b757f5c0fd9c98469f13e15e3c99
Yiqun Zhang <guiyanakuang@gmail.com>
2024-04-29 20:28:04 +0800

    :lipstick: Image shows detailed UI (#826)

--- composeApp/src/commonMain/kotlin/com/clipevery/ui/search/DetialClipDataView.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/search/DetialClipDataView.kt
@@ -6,10 +6,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.clipevery.LocalKoinApplication
 import com.clipevery.clip.ClipSearchService
+import com.clipevery.clip.item.ClipFiles
 import com.clipevery.clip.item.ClipHtml
 import com.clipevery.clip.item.ClipText
 import com.clipevery.clip.item.ClipUrl
+import com.clipevery.dao.clip.ClipType
 import com.clipevery.ui.clip.detail.ClipDetailView
+import com.clipevery.ui.clip.detail.ClipImagesDetailView
 import com.clipevery.ui.clip.detail.ClipTextDetailView
 import com.clipevery.ui.clip.detail.ClipUrlDetailView
 import com.clipevery.ui.clip.detail.HtmlToImageDetailView
@@ -23,15 +26,18 @@ fun DetialClipDataView() {
     clipSearchService.currentClipData.value?.let { clipData ->
         clipData.getClipItem()?.let {
             ClipDetailView {
-                when (it) {
-                    is ClipText -> {
-                        ClipTextDetailView(it)
+                when (clipData.clipType) {
+                    ClipType.TEXT -> {
+                        ClipTextDetailView(it as ClipText)
                     }
-                    is ClipUrl -> {
-                        ClipUrlDetailView(it)
+                    ClipType.URL -> {
+                        ClipUrlDetailView(it as ClipUrl)
                     }
-                    is ClipHtml -> {
-                        HtmlToImageDetailView(clipData, it)
+                    ClipType.HTML -> {
+                        HtmlToImageDetailView(clipData, it as ClipHtml)
+                    }
+                    ClipType.IMAGE -> {
+                        ClipImagesDetailView(clipData, it as ClipFiles)
                     }
                     else -> {
                     }
