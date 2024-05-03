1fa331ec3b5b9e6291d8dcf6e430caf16f925676
Yiqun Zhang <guiyanakuang@gmail.com>
2024-05-03 13:44:36 +0800

    :zap: html pasteboard should be previewed with text before loading

--- composeApp/src/commonMain/kotlin/com/clipevery/clip/item/ClipHtml.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/clip/item/ClipHtml.kt
@@ -1,10 +1,15 @@
 package com.clipevery.clip.item
 
+import org.jsoup.Jsoup
 import java.nio.file.Path
 
 interface ClipHtml : ClipInit {
 
     var html: String
+
+    fun getText(): String {
+        return Jsoup.parse(html).text()
+    }
 
     fun getHtmlImagePath(): Path
 }
