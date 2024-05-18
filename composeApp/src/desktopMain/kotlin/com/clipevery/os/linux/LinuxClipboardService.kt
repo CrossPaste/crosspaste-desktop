605eacff31c9d5982b55d5b4fe889871121d671b
Yiqun Zhang <guiyanakuang@gmail.com>
2024-05-18 16:13:59 +0800

    :sparkles: Implement linux ClipboardService

--- composeApp/src/desktopMain/kotlin/com/clipevery/os/linux/LinuxClipboardService.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/os/linux/LinuxClipboardService.kt
@@ -6,9 +6,20 @@ import com.clipevery.clip.TransferableConsumer
 import com.clipevery.clip.TransferableProducer
 import com.clipevery.config.ConfigManager
 import com.clipevery.dao.clip.ClipDao
+import com.clipevery.os.linux.api.X11Api
+import com.clipevery.utils.cpuDispatcher
+import com.sun.jna.NativeLong
+import com.sun.jna.platform.unix.X11
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import java.awt.Toolkit
 import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.Transferable
@@ -22,6 +33,8 @@ class LinuxClipboardService(
 ) : ClipboardService {
     override val logger: KLogger = KotlinLogging.logger {}
 
+    private var changeCount = configManager.config.lastClipboardChangeCount
+
     override var owner: Boolean = false
 
     override var ownerTransferable: Transferable? = null
@@ -30,10 +43,84 @@ class LinuxClipboardService(
 
     override val clipboardChannel: Channel<suspend () -> Unit> = Channel(Channel.UNLIMITED)
 
+    private val serviceScope = CoroutineScope(cpuDispatcher + SupervisorJob())
+
+    private var job: Job? = null
+
+    init {
+        serviceScope.launch {
+            for (task in clipboardChannel) {
+                task()
+            }
+        }
+    }
+
+    private fun run(): Job {
+        return serviceScope.launch(CoroutineName("LinuxClipboardService")) {
+            val x11 = X11Api.INSTANCE
+            x11.XOpenDisplay(null)?.let { display ->
+                try {
+                    val rootWindow = x11.XDefaultRootWindow(display)
+                    val eventMask = X11.PropertyChangeMask // 监听属性变化事件
+                    x11.XSelectInput(display, rootWindow, NativeLong(eventMask.toLong()))
+
+                    val event = X11.XEvent()
+                    while (isActive) {
+                        x11.XNextEvent(display, event)
+                        if (event.type == X11.PropertyNotify) {
+                            logger.info { "notify change event" }
+                            changeCount++
+                            val start = System.currentTimeMillis()
+                            val source = appWindowManager.getCurrentActiveAppName()
+                            val end = System.currentTimeMillis()
+
+                            val delay = 20 + start - end
+
+                            if (delay > 0) {
+                                delay(delay)
+                            }
+
+                            val contents = getContents()
+                            if (contents != ownerTransferable) {
+                                contents?.let {
+                                    launch(CoroutineName("MacClipboardServiceConsumer")) {
+                                        clipConsumer.consume(it, source, remote = false)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } finally {
+                    x11.XCloseDisplay(display)
+                }
+            }
+        }
+    }
+
+    private suspend fun getContents(): Transferable? {
+        var contents = systemClipboard.getContents(null)
+        var sum = 0L
+        var waiting = 20L
+        while (!isValidContents(contents) && sum < 1000L) {
+            delay(waiting)
+            sum += waiting
+            waiting *= 2
+            contents = systemClipboard.getContents(null)
+        }
+        return contents
+    }
+
     override fun start() {
+        if (configManager.config.enableClipboardListening) {
+            if (job?.isActive != true) {
+                job = run()
+            }
+        }
     }
 
     override fun stop() {
+        job?.cancel()
+        configManager.updateConfig { it.copy(lastClipboardChangeCount = changeCount) }
     }
 
     override fun toggle() {
