ef9aff021833d6f1322a93a790e1b67ff0cff0e1
Yiqun Zhang <guiyanakuang@gmail.com>
2024-05-22 14:05:33 +0800

    :sparkles: Detect whether the system has a proxy enabled when downloading web ico

--- composeApp/src/desktopMain/kotlin/com/clipevery/net/DesktopProxy.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/net/DesktopProxy.kt
@@ -1,17 +1,37 @@
 package com.clipevery.net
 
+import java.net.InetSocketAddress
 import java.net.Proxy
 import java.net.ProxySelector
 import java.net.URI
 
 object DesktopProxy {
 
-    init {
-        System.setProperty("java.net.useSystemProxies", "true")
-    }
-
     fun getProxy(uri: URI): Proxy {
-        val proxyList = ProxySelector.getDefault().select(uri)
-        return proxyList.firstOrNull() ?: Proxy.NO_PROXY
+        System.setProperty("java.net.useSystemProxies", "true")
+        try {
+            val proxyList = ProxySelector.getDefault().select(uri)
+            return proxyList.firstOrNull { proxy ->
+                proxy.type() == Proxy.Type.HTTP &&
+                    proxy.address() != null &&
+                    proxy.address() is InetSocketAddress
+            } ?: run {
+                val proxyName =
+                    if (uri.scheme == "http") {
+                        "HTTP_PROXY"
+                    } else {
+                        "HTTPS_PROXY"
+                    }
+                System.getenv(proxyName)?.let {
+                    val proxyHost = it.split(":")[0]
+                    val proxyPort = it.split(":")[1].toInt()
+                    Proxy(Proxy.Type.HTTP, InetSocketAddress(proxyHost, proxyPort))
+                } ?: run {
+                    Proxy.NO_PROXY
+                }
+            }
+        } finally {
+            System.setProperty("java.net.useSystemProxies", "false")
+        }
     }
 }
