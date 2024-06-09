d718a9b4570c9c21446051f728327369fdcafbb9
Yiqun Zhang <guiyanakuang@gmail.com>
2024-06-09 20:01:34 +0800

    :zap: Optimize BonjourService startup and exit performance (#1194)

--- composeApp/src/desktopMain/kotlin/com/clipevery/net/DesktopClipBonjourService.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/net/DesktopClipBonjourService.kt
@@ -6,6 +6,14 @@ import com.clipevery.dto.sync.SyncInfo
 import com.clipevery.endpoint.EndpointInfoFactory
 import com.clipevery.sync.DeviceManager
 import com.clipevery.utils.TxtRecordUtils
+import com.clipevery.utils.ioDispatcher
+import io.ktor.util.collections.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.net.InetAddress
 import javax.jmdns.JmDNS
 import javax.jmdns.ServiceInfo
@@ -21,7 +29,11 @@ class DesktopClipBonjourService(
         private const val SERVICE_TYPE = "_clipeveryService._tcp.local."
     }
 
-    private val jmdnsMap: MutableMap<String, JmDNS> = mutableMapOf()
+    private val supervisorJob = SupervisorJob()
+
+    private val scope = CoroutineScope(ioDispatcher + supervisorJob)
+
+    private val jmdnsMap: MutableMap<String, JmDNS> = ConcurrentMap()
 
     override fun registerService(): ClipBonjourService {
         val endpointInfo = endpointInfoFactory.createEndpointInfo()
@@ -33,33 +45,40 @@ class DesktopClipBonjourService(
         val serviceListener = deviceManager as ServiceListener
 
         for (hostInfo in endpointInfo.hostInfoList) {
-            val hostAddress = hostInfo.hostAddress
-            val jmDNS: JmDNS = JmDNS.create(InetAddress.getByName(hostAddress))
-            jmdnsMap.putIfAbsent(hostAddress, jmDNS)
-            val serviceInfo =
-                ServiceInfo.create(
-                    SERVICE_TYPE,
-                    "clipevery@${appInfo.appInstanceId}@${hostAddress.replace(".", "_")}",
-                    endpointInfo.port,
-                    0,
-                    0,
-                    txtRecordDict,
-                )
-            jmDNS.addServiceListener(SERVICE_TYPE, serviceListener)
-            jmDNS.registerService(serviceInfo)
+            scope.launch {
+                val hostAddress = hostInfo.hostAddress
+                jmdnsMap.computeIfAbsent(hostAddress) {
+                    val jmDNS = JmDNS.create(InetAddress.getByName(hostAddress))
+                    val serviceInfo =
+                        ServiceInfo.create(
+                            SERVICE_TYPE,
+                            "clipevery@${appInfo.appInstanceId}@${hostAddress.replace(".", "_")}",
+                            endpointInfo.port,
+                            0,
+                            0,
+                            txtRecordDict,
+                        )
+                    jmDNS.addServiceListener(SERVICE_TYPE, serviceListener)
+                    jmDNS.registerService(serviceInfo)
+                    jmDNS
+                }
+            }
         }
         return this
     }
 
     override fun unregisterService(): ClipBonjourService {
-        val iterator = jmdnsMap.iterator()
-        while (iterator.hasNext()) {
-            val entry = iterator.next()
-            val jmDNS = entry.value
-            jmDNS.unregisterAllServices()
-            jmDNS.close()
-            iterator.remove()
-        }
+        val deferred =
+            scope.async {
+                jmdnsMap.values.map { jmDNS ->
+                    async {
+                        jmDNS.unregisterAllServices()
+                        jmDNS.close()
+                    }
+                }.awaitAll()
+            }
+        runBlocking { deferred.await() }
+        jmdnsMap.clear()
         return this
     }
 }
