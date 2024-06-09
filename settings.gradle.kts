a8beda94d81128a37ec9cada0f621d882ac3df16
Yiqun Zhang <guiyanakuang@gmail.com>
2024-06-09 15:19:45 +0800

    :zap: Prefer google as plugin repository (#1190)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,15 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
