a6d0356bc3c59a115ea59263b160a58db7e37102
Yiqun Zhang <guiyanakuang@gmail.com>
2024-06-10 12:24:51 +0800

    :bug: Fix crash caused by failure to load file preview icon file

--- composeApp/src/commonMain/kotlin/com/clipevery/ui/base/AsyncView.kt
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/base/AsyncView.kt
@@ -1,44 +1,9 @@
 package com.clipevery.ui.base
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.ProduceStateScope
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import com.clipevery.utils.ioDispatcher
-import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
-
-class ClipProduceStateScopeImpl<T>(
-    state: MutableState<T>,
-    override val coroutineContext: CoroutineContext,
-) : ProduceStateScope<T>, MutableState<T> by state {
-
-    override suspend fun awaitDispose(onDispose: () -> Unit): Nothing {
-        try {
-            suspendCancellableCoroutine<Nothing> { }
-        } finally {
-            onDispose()
-        }
-    }
-}
-
-@Composable
-fun <T> clipProduceState(
-    initialValue: T,
-    key1: Any?,
-    producer: suspend ProduceStateScope<T>.() -> Unit,
-): State<T> {
-    val result = remember(key1) { mutableStateOf(initialValue) }
-    LaunchedEffect(key1) {
-        ClipProduceStateScopeImpl(result, coroutineContext).producer()
-    }
-    return result
-}
 
 @Composable
 fun AsyncView(
@@ -59,39 +24,4 @@ fun AsyncView(
     }
 
     loadFor(state)
-}
-
-object LoadingStateData : LoadStateData {
-    override fun getLoadState(): LoadState {
-        return LoadState.Loading
-    }
-}
-
-class ErrorStateData(val throwable: Throwable) : LoadStateData {
-    override fun getLoadState(): LoadState {
-        return LoadState.Error
-    }
-}
-
-interface LoadStateData {
-
-    fun getLoadState(): LoadState
-
-    fun isLoading(): Boolean {
-        return getLoadState() == LoadState.Loading
-    }
-
-    fun isSuccess(): Boolean {
-        return getLoadState() == LoadState.Success
-    }
-
-    fun isError(): Boolean {
-        return getLoadState() == LoadState.Error
-    }
-}
-
-enum class LoadState {
-    Loading,
-    Success,
-    Error,
 }
---
+++ composeApp/src/commonMain/kotlin/com/clipevery/ui/base/LoadData.kt
@@ -0,0 +1,70 @@
+package com.clipevery.ui.base
+
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.loadSvgPainter
+import androidx.compose.ui.res.loadXmlImageVector
+import androidx.compose.ui.unit.Density
+import com.clipevery.utils.FileExtUtils
+import com.clipevery.utils.getResourceUtils
+import org.xml.sax.InputSource
+import java.nio.file.Path
+import kotlin.io.path.exists
+import kotlin.io.path.extension
+import kotlin.io.path.inputStream
+import kotlin.io.path.pathString
+
+fun loadImageData(
+    path: Path,
+    density: Density,
+    thumbnail: Boolean = false,
+): LoadStateData {
+    try {
+        val toPainterImage =
+            when (path.pathString.substringAfterLast(".")) {
+                "svg" -> SvgResourceToPainter(path, path.inputStream().buffered().use { loadSvgPainter(it, density) })
+                "xml" ->
+                    XmlResourceToPainter(
+                        path.inputStream().buffered().use { loadXmlImageVector(InputSource(it), density) },
+                    )
+
+                else -> {
+                    if (thumbnail) {
+                        val thumbnailPath = getThumbnailPath(path)
+                        if (!thumbnailPath.exists()) {
+                            createThumbnail(path)
+                        }
+                        ImageBitmapToPainter(
+                            path,
+                            thumbnailPath.inputStream().buffered().use { it.use(::loadImageBitmap) },
+                        )
+                    } else {
+                        ImageBitmapToPainter(path, path.inputStream().buffered().use { it.use(::loadImageBitmap) })
+                    }
+                }
+            }
+        return LoadImageData(path, toPainterImage)
+    } catch (e: Exception) {
+        return ErrorStateData(e)
+    }
+}
+
+fun loadIconData(
+    filePath: Path,
+    isFile: Boolean,
+    density: Density,
+): LoadStateData {
+    try {
+        if (isFile) {
+            val extension = filePath.extension
+            FileExtUtils.getExtPreviewImage(extension)?.let {
+                return LoadImageData(extension, ImageBitmapToPainter(extension, it))
+            } ?: run {
+                return LoadIconData("file", getResourceUtils().loadPainter("icon/clip/file.svg", density))
+            }
+        } else {
+            return LoadIconData("dir", getResourceUtils().loadPainter("icon/clip/folder.svg", density))
+        }
+    } catch (e: Exception) {
+        return ErrorStateData(e)
+    }
+}
