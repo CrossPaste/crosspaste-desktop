cf85b1f105b393470cead5585e3c49e3de11a9cd
Yiqun Zhang <guiyanakuang@gmail.com>
2024-06-18 22:32:15 +0800

    :sparkles: Support for encrypted file transfer

--- composeApp/src/desktopMain/kotlin/com/clipevery/net/plugin/SignalServerEncryptPlugin.kt
+++ composeApp/src/desktopMain/kotlin/com/clipevery/net/plugin/SignalServerEncryptPlugin.kt
@@ -2,9 +2,12 @@ package com.clipevery.net.plugin
 
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import io.ktor.util.*
+import io.ktor.utils.io.*
 import org.signal.libsignal.protocol.SessionCipher
 import org.signal.libsignal.protocol.SignalProtocolAddress
 import org.signal.libsignal.protocol.state.SignalProtocolStore
@@ -19,22 +22,36 @@ val SIGNAL_SERVER_ENCRYPT_PLUGIN: ApplicationPlugin<SignalConfig> =
 
         val signalProtocolStore: SignalProtocolStore = pluginConfig.signalProtocolStore
 
-        onCallRespond { call, message ->
+        onCallRespond { call ->
             val headers = call.request.headers
             headers["appInstanceId"]?.let { appInstanceId ->
                 headers["signal"]?.let {
                     logger.debug { "signal server encrypt $appInstanceId" }
-                    call.response.pipeline.intercept(ApplicationSendPipeline.Before) { message ->
-                        if (message is OutgoingContent.ByteArrayContent) {
-                            val bytes = message.bytes()
-                            val signalProtocolAddress = SignalProtocolAddress(appInstanceId, 1)
-                            val sessionCipher = SessionCipher(signalProtocolStore, signalProtocolAddress)
-                            val ciphertextMessage = sessionCipher.encrypt(bytes)
-                            val byteArrayContent = ByteArrayContent(ciphertextMessage.serialize(), contentType = message.contentType)
-                            proceedWith(byteArrayContent)
-                        } else {
-                            proceed()
-                        }
+                    call.response.pipeline.intercept(ApplicationSendPipeline.Transform) { message ->
+                        val originalContent: Pair<ByteArray, ContentType?> =
+                            when (message) {
+                                is OutgoingContent.ByteArrayContent -> Pair(message.bytes(), message.contentType)
+                                is OutgoingContent.ReadChannelContent -> Pair(message.readFrom().toByteArray(), message.contentType)
+                                is OutgoingContent.WriteChannelContent -> {
+                                    val channel = ByteChannel(true)
+                                    message.writeTo(channel)
+                                    Pair(channel.toByteArray(), message.contentType)
+                                }
+
+                                is OutgoingContent.NoContent -> Pair(ByteArray(0), null)
+                                else -> Pair(message.toString().toByteArray(), null)
+                            }
+
+                        val signalProtocolAddress = SignalProtocolAddress(appInstanceId, 1)
+                        val sessionCipher = SessionCipher(signalProtocolStore, signalProtocolAddress)
+                        val ciphertextMessage = sessionCipher.encrypt(originalContent.first)
+                        val byteArrayContent =
+                            ByteArrayContent(
+                                ciphertextMessage.serialize(),
+                                contentType = originalContent.second,
+                            )
+
+                        proceedWith(byteArrayContent)
                     }
                 }
             }
