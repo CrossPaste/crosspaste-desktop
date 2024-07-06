ad1ca14fa63a588336bd01455e452a7a15efead3
Yiqun Zhang <guiyanakuang@gmail.com>
2024-07-06 17:40:51 +0800

    :lipstick: Pasteboard menu adapted to the new theme (#1379)

--- composeApp/src/commonMain/kotlin/com/crosspaste/ui/paste/preview/PasteMenuView.kt
+++ composeApp/src/commonMain/kotlin/com/crosspaste/ui/paste/preview/PasteMenuView.kt
@@ -1,16 +1,18 @@
 package com.crosspaste.ui.paste.preview
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.onClick
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -62,7 +64,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun PasteMenuView(
     pasteData: PasteData,
@@ -83,6 +85,11 @@ fun PasteMenuView(
     var job: Job? by remember { mutableStateOf(null) }
 
     var showPopup by remember { mutableStateOf(false) }
+
+    var hoverMenu by remember { mutableStateOf(false) }
+    var hoverCopy by remember { mutableStateOf(false) }
+    var hoverFavorite by remember { mutableStateOf(false) }
+    var hoverSource by remember { mutableStateOf(false) }
 
     fun startShowing() {
         if (job?.isActive == true) { // Don't restart the job if it's already active
@@ -127,132 +134,262 @@ fun PasteMenuView(
                 .onPointerEvent(PointerEventType.Exit) {
                     hideIfNotHovered(parentBounds.topLeft + it.position)
                 }
-                .background(if (showMenu) MaterialTheme.colors.surface.copy(alpha = 0.12f) else Color.Transparent)
-                .padding(vertical = 5.dp),
+                .clip(RoundedCornerShape(5.dp))
+                .background(if (showMenu) MaterialTheme.colors.surface.copy(0.36f) else Color.Transparent),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
         PasteTooltipAreaView(
+            Modifier.fillMaxWidth().height(25.dp),
             text = copywriter.getText("Menu"),
         ) {
-            Icon(
-                Icons.Outlined.MoreVert,
-                contentDescription = "info",
+            Box(
                 modifier =
-                    Modifier.size(18.dp)
-                        .clickable {
-                            showPopup = !showPopup
-                        },
-                tint = MaterialTheme.colors.primary,
-            )
-        }
-
-        if (showPopup) {
-            Popup(
-                alignment = Alignment.TopEnd,
-                offset =
-                    IntOffset(
-                        with(density) { ((-30).dp).roundToPx() },
-                        with(density) { (5.dp).roundToPx() },
-                    ),
-                onDismissRequest = {
-                    if (showPopup) {
-                        showPopup = false
-                        showMenu = false
-                        toShow(false)
-                    }
-                },
-                properties =
-                    PopupProperties(
-                        focusable = true,
-                        dismissOnBackPress = true,
-                        dismissOnClickOutside = true,
-                    ),
+                    Modifier.fillMaxWidth()
+                        .onPointerEvent(
+                            eventType = PointerEventType.Enter,
+                            onEvent = {
+                                hoverMenu = true
+                            },
+                        )
+                        .onPointerEvent(
+                            eventType = PointerEventType.Exit,
+                            onEvent = {
+                                hoverMenu = false
+                            },
+                        ),
+                contentAlignment = Alignment.Center,
             ) {
                 Box(
                     modifier =
-                        Modifier
-                            .wrapContentSize()
-                            .background(Color.Transparent)
-                            .shadow(15.dp),
-                ) {
-                    val menuTexts =
-                        arrayOf(
-                            copywriter.getText("Open"),
-                            copywriter.getText("Delete"),
-                        )
-
-                    val maxWidth = getMenWidth(menuTexts)
-
-                    Column(
-                        modifier =
-                            Modifier
-                                .width(maxWidth)
-                                .wrapContentHeight()
-                                .clip(RoundedCornerShape(5.dp))
-                                .background(MaterialTheme.colors.surface),
-                    ) {
-                        MenuItem(copywriter.getText("Open")) {
-                            uiSupport.openPasteData(pasteData)
-                            showPopup = false
-                            showMenu = false
-                            toShow(false)
-                        }
-                        MenuItem(copywriter.getText("Delete")) {
-                            runBlocking {
-                                pasteDao.markDeletePasteData(pasteData.id)
-                            }
-                            showPopup = false
-                            showMenu = false
-                            toShow(false)
-                        }
-                    }
-                }
+                        Modifier.fillMaxSize()
+                            .clip(RoundedCornerShape(6.dp))
+                            .background(
+                                if (hoverMenu) {
+                                    MaterialTheme.colors.surface
+                                } else {
+                                    Color.Transparent
+                                },
+                            ),
+                ) {}
+                Icon(
+                    Icons.Outlined.MoreVert,
+                    contentDescription = "info",
+                    modifier =
+                        Modifier.size(18.dp)
+                            .onClick {
+                                showPopup = !showPopup
+                            },
+                    tint = MaterialTheme.colors.primary,
+                )
             }
         }
 
         if (showMenu) {
             PasteTooltipAreaView(
+                Modifier.fillMaxWidth().height(25.dp),
                 text = copywriter.getText("Copy"),
             ) {
-                Icon(
+                Box(
                     modifier =
-                        Modifier.size(16.dp).clickable {
-                            runBlocking {
-                                pasteboardService.tryWritePasteboard(pasteData, localOnly = true, filterFile = false)
-                            }
-                            toastManager.setToast(
-                                Toast(
-                                    MessageType.Success,
-                                    copywriter.getText("Copy_Successful"),
-                                    3000,
-                                ),
+                        Modifier.fillMaxWidth()
+                            .onPointerEvent(
+                                eventType = PointerEventType.Enter,
+                                onEvent = {
+                                    hoverCopy = true
+                                },
                             )
-                        },
-                    painter = clipboard(),
-                    contentDescription = "Copy",
-                    tint = MaterialTheme.colors.onBackground,
-                )
+                            .onPointerEvent(
+                                eventType = PointerEventType.Exit,
+                                onEvent = {
+                                    hoverCopy = false
+                                },
+                            ),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Box(
+                        modifier =
+                            Modifier.fillMaxSize()
+                                .clip(RoundedCornerShape(6.dp))
+                                .background(
+                                    if (hoverCopy) {
+                                        MaterialTheme.colors.surface
+                                    } else {
+                                        Color.Transparent
+                                    },
+                                ),
+                    ) {}
+
+                    Icon(
+                        modifier =
+                            Modifier.size(16.dp).onClick {
+                                runBlocking {
+                                    pasteboardService.tryWritePasteboard(
+                                        pasteData,
+                                        localOnly = true,
+                                        filterFile = false,
+                                    )
+                                }
+                                toastManager.setToast(
+                                    Toast(
+                                        MessageType.Success,
+                                        copywriter.getText("Copy_Successful"),
+                                        3000,
+                                    ),
+                                )
+                            },
+                        painter = clipboard(),
+                        contentDescription = "Copy",
+                        tint = MaterialTheme.colors.onBackground,
+                    )
+                }
             }
 
             PasteTooltipAreaView(
+                Modifier.fillMaxWidth().height(25.dp),
                 text = copywriter.getText(if (pasteData.favorite) "Delete_Favorite" else "Favorite"),
             ) {
-                Icon(
+                Box(
                     modifier =
-                        Modifier.size(16.dp).clickable {
-                            pasteDao.setFavorite(pasteData.id, !pasteData.favorite)
-                        },
-                    painter = if (pasteData.favorite) favorite() else noFavorite(),
-                    contentDescription = "Favorite",
-                    tint = favoriteColor(),
-                )
+                        Modifier.fillMaxWidth()
+                            .onPointerEvent(
+                                eventType = PointerEventType.Enter,
+                                onEvent = {
+                                    hoverFavorite = true
+                                },
+                            )
+                            .onPointerEvent(
+                                eventType = PointerEventType.Exit,
+                                onEvent = {
+                                    hoverFavorite = false
+                                },
+                            ),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Box(
+                        modifier =
+                            Modifier.fillMaxSize()
+                                .clip(RoundedCornerShape(6.dp))
+                                .background(
+                                    if (hoverFavorite) {
+                                        MaterialTheme.colors.surface
+                                    } else {
+                                        Color.Transparent
+                                    },
+                                ),
+                    ) {}
+
+                    Icon(
+                        modifier =
+                            Modifier.size(16.dp).onClick {
+                                pasteDao.setFavorite(pasteData.id, !pasteData.favorite)
+                            },
+                        painter = if (pasteData.favorite) favorite() else noFavorite(),
+                        contentDescription = "Favorite",
+                        tint = favoriteColor(),
+                    )
+                }
             }
 
             PasteTooltipAreaView(
+                Modifier.fillMaxWidth().height(25.dp),
                 text = copywriter.getText(getTypeText(pasteData.pasteType)),
             ) {
-                PasteTypeIconView(pasteData, size = 16.dp)
+                Box(
+                    modifier =
+                        Modifier.fillMaxWidth()
+                            .onPointerEvent(
+                                eventType = PointerEventType.Enter,
+                                onEvent = {
+                                    hoverSource = true
+                                },
+                            )
+                            .onPointerEvent(
+                                eventType = PointerEventType.Exit,
+                                onEvent = {
+                                    hoverSource = false
+                                },
+                            ),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Box(
+                        modifier =
+                            Modifier.fillMaxSize()
+                                .clip(RoundedCornerShape(6.dp))
+                                .background(
+                                    if (hoverSource) {
+                                        MaterialTheme.colors.surface
+                                    } else {
+                                        Color.Transparent
+                                    },
+                                ),
+                    ) {}
+                    PasteTypeIconView(pasteData, size = 16.dp)
+                }
+            }
+        }
+    }
+
+    if (showPopup) {
+        Popup(
+            alignment = Alignment.TopEnd,
+            offset =
+                IntOffset(
+                    with(density) { ((-30).dp).roundToPx() },
+                    with(density) { (5.dp).roundToPx() },
+                ),
+            onDismissRequest = {
+                if (showPopup) {
+                    showPopup = false
+                    showMenu = false
+                    toShow(false)
+                }
+            },
+            properties =
+                PopupProperties(
+                    focusable = true,
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true,
+                ),
+        ) {
+            Box(
+                modifier =
+                    Modifier
+                        .wrapContentSize()
+                        .background(Color.Transparent)
+                        .shadow(15.dp),
+            ) {
+                val menuTexts =
+                    arrayOf(
+                        copywriter.getText("Open"),
+                        copywriter.getText("Delete"),
+                    )
+
+                val maxWidth = getMenWidth(menuTexts)
+
+                Column(
+                    modifier =
+                        Modifier
+                            .width(maxWidth)
+                            .wrapContentHeight()
+                            .clip(RoundedCornerShape(5.dp))
+                            .background(MaterialTheme.colors.surface),
+                ) {
+                    MenuItem(copywriter.getText("Open")) {
+                        uiSupport.openPasteData(pasteData)
+                        showPopup = false
+                        showMenu = false
+                        toShow(false)
+                    }
+                    MenuItem(copywriter.getText("Delete")) {
+                        runBlocking {
+                            pasteDao.markDeletePasteData(pasteData.id)
+                        }
+                        showPopup = false
+                        showMenu = false
+                        toShow(false)
+                    }
+                }
             }
         }
     }
