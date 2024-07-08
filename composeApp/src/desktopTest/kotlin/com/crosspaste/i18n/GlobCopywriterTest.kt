7e904ca0a4b23252161610ee9d0f058f9cd772c1
Yiqun Zhang <guiyanakuang@gmail.com>
2024-07-08 11:17:00 +0800

    :bug: Fix configuration persistence issue and support error message

--- composeApp/src/desktopTest/kotlin/com/crosspaste/i18n/GlobCopywriterTest.kt
+++ composeApp/src/desktopTest/kotlin/com/crosspaste/i18n/GlobCopywriterTest.kt
@@ -3,6 +3,7 @@ package com.crosspaste.i18n
 import com.crosspaste.config.DefaultConfigManager
 import com.crosspaste.i18n.GlobalCopywriterImpl.Companion.EN
 import com.crosspaste.presist.DesktopOneFilePersist
+import com.crosspaste.ui.base.DesktopToastManager
 import java.nio.file.Files
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -14,7 +15,10 @@ class GlobCopywriterTest {
         val configDirPath = Files.createTempDirectory("configDir")
         configDirPath.toFile().deleteOnExit()
         val configPath = configDirPath.resolve("appConfig.json")
-        val configManager = DefaultConfigManager(DesktopOneFilePersist(configPath))
+
+        lateinit var configManager: DefaultConfigManager
+
+        configManager = DefaultConfigManager(DesktopOneFilePersist(configPath), DesktopToastManager(), lazy { GlobalCopywriterImpl(configManager) })
 
         configManager.updateConfig { config -> config.copy(language = "") }
 
