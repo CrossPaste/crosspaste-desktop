e247bc4b5c53049dac6ddd8794c890723fbfefad
Yiqun Zhang <guiyanakuang@gmail.com>
2024-07-12 20:09:15 +0800

    :bug: Fix bug where pasting text fail

--- composeApp/src/desktopMain/kotlin/com/crosspaste/paste/DesktopPasteDataFlavor.kt
+++ composeApp/src/desktopMain/kotlin/com/crosspaste/paste/DesktopPasteDataFlavor.kt
@@ -2,7 +2,21 @@ package com.crosspaste.paste
 
 import java.awt.datatransfer.DataFlavor
 
-data class DesktopPasteDataFlavor(val dataFlavor: DataFlavor) : PasteDataFlavor
+data class DesktopPasteDataFlavor(val dataFlavor: DataFlavor) : PasteDataFlavor {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is DesktopPasteDataFlavor) return false
+
+        if (dataFlavor != other.dataFlavor) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return dataFlavor.hashCode()
+    }
+}
 
 fun DataFlavor.toPasteDataFlavor(): PasteDataFlavor {
     return DesktopPasteDataFlavor(this)
