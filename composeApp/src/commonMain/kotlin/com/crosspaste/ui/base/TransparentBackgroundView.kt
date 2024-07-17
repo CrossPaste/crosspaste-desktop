3178880b71994ae3f9fc75d4824715802faec11c
Yiqun Zhang <guiyanakuang@gmail.com>
2024-07-17 17:51:13 +0800

    :bug: Fix the issue where the transparent background did not adapt to the theme (#1493)

--- composeApp/src/commonMain/kotlin/com/crosspaste/ui/base/TransparentBackgroundView.kt
+++ composeApp/src/commonMain/kotlin/com/crosspaste/ui/base/TransparentBackgroundView.kt
@@ -1,6 +1,7 @@
 package com.crosspaste.ui.base
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -10,6 +11,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun TransparentBackground(modifier: Modifier = Modifier) {
+    val background = MaterialTheme.colors.background
     Canvas(modifier = modifier) {
         val stepSize = 5.dp.toPx()
         val columns = (size.width / stepSize).toInt()
@@ -17,7 +19,7 @@ fun TransparentBackground(modifier: Modifier = Modifier) {
 
         for (i in 0 until columns) {
             for (j in 0 until rows) {
-                val color = if ((i + j) % 2 == 0) Color.LightGray else Color.White
+                val color = if ((i + j) % 2 == 0) Color.LightGray else background
                 drawRect(
                     color = color,
                     topLeft = Offset(i * stepSize, j * stepSize),
