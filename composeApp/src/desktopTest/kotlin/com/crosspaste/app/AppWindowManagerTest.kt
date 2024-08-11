a0a20d083ba4b905acf5d600b9a1e928ad9eb577
Yiqun Zhang <guiyanakuang@gmail.com>
2024-08-11 09:35:30 +0800

    :white_check_mark: Rename test classes to follow naming conventions (#1659)

--- composeApp/src/desktopTest/kotlin/com/crosspaste/app/AppWindowManagerTest.kt
+++ composeApp/src/desktopTest/kotlin/com/crosspaste/app/AppWindowManagerTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
-class TestAppWindowManager {
+class AppWindowManagerTest {
 
     @Test
     fun testMockTestAppWindowManager() {
