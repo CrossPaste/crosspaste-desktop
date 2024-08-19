620e6bca02b79053d8d385f6272e5211ae30a2ff
Yiqun Zhang <guiyanakuang@gmail.com>
2024-08-19 13:25:51 +0800

    :zap: Optimize package size: Avoid bundling chrome-driver and chrome-headless-shell

--- composeApp/src/desktopMain/kotlin/com/crosspaste/task/Html2ImageTaskExecutor.kt
+++ composeApp/src/desktopMain/kotlin/com/crosspaste/task/Html2ImageTaskExecutor.kt
@@ -13,7 +13,11 @@ import com.crosspaste.task.extra.BaseExtraInfo
 import com.crosspaste.ui.paste.preview.getPasteItem
 import com.crosspaste.utils.TaskUtils
 import com.crosspaste.utils.TaskUtils.createFailurePasteTaskResult
+import com.crosspaste.utils.ioDispatcher
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
@@ -30,9 +34,14 @@ class Html2ImageTaskExecutor(
 
     private val mutex = Mutex()
 
+    private val chromeServiceDeferred: Deferred<ChromeService> =
+        CoroutineScope(ioDispatcher).async {
+            lazyChromeService.value
+        }
+
     override suspend fun doExecuteTask(pasteTask: PasteTask): PasteTaskResult {
         mutex.withLock {
-            val chromeService = lazyChromeService.value
+            val chromeService = chromeServiceDeferred.await()
             try {
                 pasteDao.getPasteData(pasteTask.pasteDataId!!)?.let { pasteData ->
                     pasteData.getPasteItem()?.let { pasteItem ->
