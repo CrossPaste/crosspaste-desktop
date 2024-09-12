f768e786fffbf80cf4dccf7dee56baa425012ef1
Yiqun Zhang <guiyanakuang@gmail.com>
2024-09-12 21:33:07 +0800

    :bug: Failure to copy images exceeding backup file threshold

--- composeApp/src/desktopTest/kotlin/com/crosspaste/image/ImageDataLoaderTest.kt
+++ composeApp/src/desktopTest/kotlin/com/crosspaste/image/ImageDataLoaderTest.kt
@@ -1,18 +1,38 @@
 package com.crosspaste.image
 
 import androidx.compose.ui.unit.Density
+import com.crosspaste.config.DefaultConfigManager
+import com.crosspaste.path.DevelopmentPlatformUserDataPathProvider
+import com.crosspaste.path.UserDataPathProvider
+import com.crosspaste.presist.OneFilePersist
+import okio.Path.Companion.toOkioPath
 import okio.Path.Companion.toPath
+import java.nio.file.Files
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class ImageDataLoaderTest {
 
-    private val imageDataLoader = getImageDataLoader()
-
     private val density = Density(1f, 1f)
 
     @Test
     fun testLoadImage() {
+        val configDirPath = Files.createTempDirectory("configDir").toOkioPath()
+        configDirPath.toFile().deleteOnExit()
+        val configPath = configDirPath.resolve("appConfig.json")
+
+        val imageDataLoader =
+            DesktopImageDataLoader(
+                DesktopThumbnailLoader(
+                    UserDataPathProvider(
+                        DefaultConfigManager(
+                            OneFilePersist(configPath),
+                        ),
+                        DevelopmentPlatformUserDataPathProvider(),
+                    ),
+                ),
+            )
+
         val crosspasteIconPath =
             this::class.java.classLoader
                 .getResource("crosspaste_icon.png")?.path?.toPath()!!
