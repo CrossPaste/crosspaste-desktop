00ead5aff02e95ea42d2e1915fe198d2517e8703
Yiqun Zhang <guiyanakuang@gmail.com>
2024-09-13 17:13:22 +0800

    :hammer: Remove unused implementation of PasteResourceLoader

--- composeApp/src/commonMain/kotlin/com/crosspaste/ui/resource/PasteResourceLoader.kt
+++
@@ -1,7 +0,0 @@
-package com.crosspaste.ui.resource
-
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.res.ResourceLoader
-
-@OptIn(ExperimentalComposeUiApi::class)
-interface PasteResourceLoader : ResourceLoader
