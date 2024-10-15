cdf31999549e55bb24ddf03669634bc6e93e4e83
Yiqun Zhang <guiyanakuang@gmail.com>
2024-10-15 09:37:37 +0800

    :bug: Fix simple code smells (#2066)

--- composeApp/src/desktopMain/kotlin/com/crosspaste/rendering/DesktopRenderingHelper.kt
+++ composeApp/src/desktopMain/kotlin/com/crosspaste/rendering/DesktopRenderingHelper.kt
@@ -3,13 +3,10 @@ package com.crosspaste.rendering
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import com.crosspaste.app.AppSize
-import com.crosspaste.platform.getPlatform
 import java.awt.GraphicsConfiguration
 import java.awt.GraphicsEnvironment
 
 class DesktopRenderingHelper(private val appSize: AppSize) : RenderingHelper {
-
-    private val currentPlatform = getPlatform()
 
     private val globalDensity =
         GraphicsEnvironment.getLocalGraphicsEnvironment()
@@ -30,12 +27,6 @@ class DesktopRenderingHelper(private val appSize: AppSize) : RenderingHelper {
 
     private fun readScale(): Double {
         return globalDensity.density.toDouble()
-//        return if (currentPlatform.isWindows()) {
-//            val maxDpi: Int = WindowDpiHelper.getMaxDpiForMonitor()
-//            maxDpi / 96.0
-//        } else {
-//            1.0
-//        }
     }
 
     private fun readWindowDimension(): RenderingDimension {
@@ -45,13 +36,6 @@ class DesktopRenderingHelper(private val appSize: AppSize) : RenderingHelper {
         val width: Int = htmlWidthValue.value.toInt()
         val height: Int = htmlHeightValue.value.toInt()
         return RenderingDimension(width, height)
-//        return if (currentPlatform.isWindows()) {
-//            val width: Int = (htmlWidthValue * scale).toInt()
-//            val height: Int = (htmlHeightValue * scale).toInt()
-//            RenderingDimension(width, height)
-//        } else {
-//            RenderingDimension(htmlWidthValue.toInt(), htmlHeightValue.toInt())
-//        }
     }
 
     override fun refresh() {
