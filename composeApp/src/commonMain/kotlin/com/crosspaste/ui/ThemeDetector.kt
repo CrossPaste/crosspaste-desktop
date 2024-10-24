501ce5b764f1d69492e7aeb4e58eb1373256c419
Yiqun Zhang <guiyanakuang@gmail.com>
2024-10-24 19:56:53 +0800

    :sparkles: Enable ThemeDetector to get current ColorScheme (#2107)

--- composeApp/src/commonMain/kotlin/com/crosspaste/ui/ThemeDetector.kt
+++ composeApp/src/commonMain/kotlin/com/crosspaste/ui/ThemeDetector.kt
@@ -1,5 +1,9 @@
 package com.crosspaste.ui
 
+import androidx.compose.material3.ColorScheme
+import com.crosspaste.ui.CrossPasteTheme.DarkColorScheme
+import com.crosspaste.ui.CrossPasteTheme.LightColorScheme
+
 interface ThemeDetector {
     fun isSystemInDark(): Boolean
 
@@ -15,4 +19,20 @@ interface ThemeDetector {
         isFollowSystem: Boolean,
         isUserInDark: Boolean = false,
     )
+
+    fun getCurrentColorScheme(): ColorScheme {
+        return if (isFollowSystem()) {
+            if (isSystemInDark()) {
+                DarkColorScheme
+            } else {
+                LightColorScheme
+            }
+        } else {
+            if (isUserInDark()) {
+                DarkColorScheme
+            } else {
+                LightColorScheme
+            }
+        }
+    }
 }
