4a1a0e59b107184debcd8be52c83081b266bd927
Yiqun Zhang <guiyanakuang@gmail.com>
2024-10-24 20:16:51 +0800

    :hammer: Make AppTokenService multiplatform reusable (#2109)

--- composeApp/src/desktopMain/kotlin/com/crosspaste/app/DesktopAppTokenService.kt
+++ composeApp/src/desktopMain/kotlin/com/crosspaste/app/DesktopAppTokenService.kt
@@ -1,70 +1,10 @@
 package com.crosspaste.app
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import com.crosspaste.utils.ioDispatcher
-import com.crosspaste.utils.mainDispatcher
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.random.Random
-
 class DesktopAppTokenService(
     private val appWindowManager: DesktopAppWindowManager,
-) : AppTokenService {
+) : AppTokenService() {
 
-    private var startRefreshNumber: Int = 0
-
-    private var refreshTokenJob: Job? = null
-
-    private val scope = CoroutineScope(ioDispatcher)
-
-    override var showTokenProgress: Float by mutableStateOf(0.0f)
-
-    override var showToken by mutableStateOf(false)
-
-    override var token by mutableStateOf(charArrayOf('0', '0', '0', '0', '0', '0'))
-
-    override fun toShowToken() {
+    override fun preShowToken() {
         appWindowManager.setShowMainWindow(true)
-        showToken = true
-    }
-
-    private fun refreshToken() {
-        token = CharArray(6) { (Random.nextInt(10) + '0'.code).toChar() }
-        showTokenProgress = 0.0f
-    }
-
-    @Synchronized
-    override fun startRefreshToken() {
-        if (startRefreshNumber++ == 0) {
-            refreshTokenJob =
-                scope.launch(CoroutineName("RefreshToken")) {
-                    while (isActive) {
-                        withContext(mainDispatcher) {
-                            refreshToken()
-                        }
-                        while (showTokenProgress < 0.99f) {
-                            withContext(mainDispatcher) {
-                                showTokenProgress += 0.01f
-                            }
-                            delay(300)
-                        }
-                    }
-                }
-        }
-    }
-
-    @Synchronized
-    override fun stopRefreshToken() {
-        startRefreshNumber -= 1
-        if (startRefreshNumber == 0) {
-            refreshTokenJob?.cancel()
-        }
     }
 }
