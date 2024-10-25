faf2ef2a47e126a91258d93bc279576af7d7195e
Yiqun Zhang <guiyanakuang@gmail.com>
2024-10-25 16:05:25 +0800

    :hammer: Move EndpointInfoFactory to commonMain for multiplatform reuse

--- composeApp/src/commonMain/kotlin/com/crosspaste/app/EndpointInfoFactory.kt
+++ composeApp/src/commonMain/kotlin/com/crosspaste/app/EndpointInfoFactory.kt
@@ -1,9 +1,30 @@
 package com.crosspaste.app
 
 import com.crosspaste.dto.sync.EndpointInfo
+import com.crosspaste.net.PasteServer
+import com.crosspaste.platform.getPlatform
 import com.crosspaste.realm.sync.HostInfo
+import com.crosspaste.utils.DeviceUtils
+import com.crosspaste.utils.getNetUtils
 
-interface EndpointInfoFactory {
+class EndpointInfoFactory(
+    private val deviceUtils: DeviceUtils,
+    private val pasteServer: Lazy<PasteServer<*, *>>,
+) {
+    private val netUtils = getNetUtils()
 
-    fun createEndpointInfo(hostInfoFilter: (HostInfo) -> Boolean = { true }): EndpointInfo
+    fun createEndpointInfo(hostInfoFilter: (HostInfo) -> Boolean = { true }): EndpointInfo {
+        val platform = getPlatform()
+        val port = pasteServer.value.port()
+        val hostInfoList = netUtils.getHostInfoList(hostInfoFilter)
+        val deviceName = deviceUtils.getDeviceName()
+        val deviceId = deviceUtils.getDeviceId()
+        return EndpointInfo(
+            deviceId = deviceId,
+            deviceName = deviceName,
+            platform = platform,
+            hostInfoList = hostInfoList,
+            port = port,
+        )
+    }
 }
