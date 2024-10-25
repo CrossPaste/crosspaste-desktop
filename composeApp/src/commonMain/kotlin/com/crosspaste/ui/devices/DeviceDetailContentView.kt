2bbcd6ac42af4c57951c687b0d189d9b01f44a4c
Yiqun Zhang <guiyanakuang@gmail.com>
2024-10-25 16:01:56 +0800

    :art: Wrap DeviceConnectView inside Column for better layout isolation (#2111)

--- composeApp/src/commonMain/kotlin/com/crosspaste/ui/devices/DeviceDetailContentView.kt
+++ composeApp/src/commonMain/kotlin/com/crosspaste/ui/devices/DeviceDetailContentView.kt
@@ -64,14 +64,64 @@ fun DeviceDetailContentView() {
         )
     }
 
-    DeviceConnectView(syncRuntimeInfo, false) { }
+    Column {
+        DeviceConnectView(syncRuntimeInfo, false) { }
 
-    Column(
-        modifier =
-            Modifier.fillMaxSize()
-                .padding(16.dp),
-    ) {
-        if (!compatibility) {
+        Column(
+            modifier =
+                Modifier.fillMaxSize()
+                    .padding(16.dp),
+        ) {
+            if (!compatibility) {
+                Column(
+                    modifier =
+                        Modifier.wrapContentSize()
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(MaterialTheme.colorScheme.background),
+                ) {
+                    Row(
+                        modifier =
+                            Modifier.wrapContentSize()
+                                .padding(12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Icon(
+                            painter = alertCircle(),
+                            contentDescription = "Warning",
+                            tint = unmatchedColor(),
+                            modifier = Modifier.size(20.dp),
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            text =
+                                "${copywriter.getText("current_software_version")}: ${appInfo.appVersion}\n" +
+                                    "${copywriter.getText("connected_software_version")}: ${syncRuntimeInfo.appVersion}\n" +
+                                    copywriter.getText("incompatible_info"),
+                            color = unmatchedColor(),
+                            style =
+                                TextStyle(
+                                    fontWeight = FontWeight.Light,
+                                    lineHeight = 20.sp,
+                                ),
+                            fontFamily = FontFamily.SansSerif,
+                            fontSize = 14.sp,
+                            modifier = Modifier.weight(1f),
+                        )
+                    }
+                }
+            }
+
+            // Header
+            Text(
+                modifier =
+                    Modifier.wrapContentSize()
+                        .padding(start = 15.dp, bottom = 5.dp),
+                text = copywriter.getText("sync_control"),
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.headlineSmall,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 12.sp,
+            )
             Column(
                 modifier =
                     Modifier.wrapContentSize()
@@ -84,188 +134,141 @@ fun DeviceDetailContentView() {
                             .padding(12.dp),
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    Icon(
-                        painter = alertCircle(),
-                        contentDescription = "Warning",
-                        tint = unmatchedColor(),
-                        modifier = Modifier.size(20.dp),
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
                     Text(
-                        text =
-                            "${copywriter.getText("current_software_version")}: ${appInfo.appVersion}\n" +
-                                "${copywriter.getText("connected_software_version")}: ${syncRuntimeInfo.appVersion}\n" +
-                                copywriter.getText("incompatible_info"),
-                        color = unmatchedColor(),
-                        style =
-                            TextStyle(
-                                fontWeight = FontWeight.Light,
-                                lineHeight = 20.sp,
-                            ),
+                        text = "${copywriter.getText("allow_send_to")} ${syncRuntimeInfo.getDeviceDisplayName()}",
+                        color = MaterialTheme.colorScheme.onBackground,
+                        style = TextStyle(fontWeight = FontWeight.Light),
                         fontFamily = FontFamily.SansSerif,
                         fontSize = 14.sp,
                         modifier = Modifier.weight(1f),
                     )
+                    CustomSwitch(
+                        modifier =
+                            Modifier.align(Alignment.CenterVertically)
+                                .width(32.dp)
+                                .height(20.dp),
+                        checked = syncRuntimeInfo.allowSend,
+                        onCheckedChange = {
+                            runBlocking {
+                                syncManager.getSyncHandlers()[syncRuntimeInfo.appInstanceId]
+                                    ?.update {
+                                        this.allowSend = it
+                                    }?.let {
+                                        syncRuntimeInfo = it
+                                    }
+                            }
+                        },
+                        checkedThumbColor = connectedColor(),
+                    )
                 }
-            }
-        }
 
-        // Header
-        Text(
-            modifier =
-                Modifier.wrapContentSize()
-                    .padding(start = 15.dp, bottom = 5.dp),
-            text = copywriter.getText("sync_control"),
-            color = MaterialTheme.colorScheme.onBackground,
-            style = MaterialTheme.typography.headlineSmall,
-            fontFamily = FontFamily.SansSerif,
-            fontSize = 12.sp,
-        )
-        Column(
-            modifier =
-                Modifier.wrapContentSize()
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(MaterialTheme.colorScheme.background),
-        ) {
-            Row(
-                modifier =
-                    Modifier.wrapContentSize()
-                        .padding(12.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "${copywriter.getText("allow_send_to")} ${syncRuntimeInfo.getDeviceDisplayName()}",
-                    color = MaterialTheme.colorScheme.onBackground,
-                    style = TextStyle(fontWeight = FontWeight.Light),
-                    fontFamily = FontFamily.SansSerif,
-                    fontSize = 14.sp,
-                    modifier = Modifier.weight(1f),
-                )
-                CustomSwitch(
-                    modifier =
-                        Modifier.align(Alignment.CenterVertically)
-                            .width(32.dp)
-                            .height(20.dp),
-                    checked = syncRuntimeInfo.allowSend,
-                    onCheckedChange = {
-                        runBlocking {
-                            syncManager.getSyncHandlers()[syncRuntimeInfo.appInstanceId]
-                                ?.update {
-                                    this.allowSend = it
-                                }?.let {
-                                    syncRuntimeInfo = it
-                                }
-                        }
-                    },
-                    checkedThumbColor = connectedColor(),
-                )
-            }
+                HorizontalDivider(modifier = Modifier.padding(start = 15.dp))
 
-            HorizontalDivider(modifier = Modifier.padding(start = 15.dp))
-
-            Row(
-                modifier =
-                    Modifier.wrapContentSize()
-                        .padding(12.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "${copywriter.getText("allow_receive_from")} ${syncRuntimeInfo.getDeviceDisplayName()}",
-                    color = MaterialTheme.colorScheme.onBackground,
-                    style = TextStyle(fontWeight = FontWeight.Light),
-                    fontFamily = FontFamily.SansSerif,
-                    fontSize = 14.sp,
-                    modifier = Modifier.weight(1f),
-                )
-                CustomSwitch(
-                    modifier =
-                        Modifier.align(Alignment.CenterVertically)
-                            .width(32.dp)
-                            .height(20.dp),
-                    checked = syncRuntimeInfo.allowReceive,
-                    onCheckedChange = {
-                        runBlocking {
-                            syncManager.getSyncHandlers()[syncRuntimeInfo.appInstanceId]
-                                ?.update {
-                                    this.allowReceive = it
-                                }?.let {
-                                    syncRuntimeInfo = it
-                                }
-                        }
-                    },
-                    checkedThumbColor = connectedColor(),
-                )
-            }
-        }
-
-        Spacer(Modifier.height(16.dp))
-
-        var maxWidth by remember { mutableStateOf(0.dp) }
-
-        val properties =
-            remember(syncRuntimeInfo) {
-                arrayOf(
-                    Pair("app_version", syncRuntimeInfo.appVersion),
-                    Pair("user_name", syncRuntimeInfo.userName),
-                    Pair("device_id", syncRuntimeInfo.deviceId),
-                    Pair("arch", syncRuntimeInfo.platformArch),
-                    Pair("connect_host", syncRuntimeInfo.connectHostAddress ?: ""),
-                    Pair("port", syncRuntimeInfo.port.toString()),
-                )
-            }
-
-        val textStyle =
-            TextStyle(
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Normal,
-            )
-
-        for (property in properties) {
-            maxWidth = maxOf(maxWidth, measureTextWidth(copywriter.getText(property.first), textStyle))
-        }
-
-        Text(
-            modifier =
-                Modifier.wrapContentSize()
-                    .padding(start = 15.dp, bottom = 5.dp),
-            text = copywriter.getText("base_info"),
-            color = MaterialTheme.colorScheme.onBackground,
-            fontFamily = FontFamily.SansSerif,
-            style = MaterialTheme.typography.headlineSmall,
-            fontSize = 12.sp,
-        )
-
-        Column(
-            modifier =
-                Modifier.wrapContentSize()
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(MaterialTheme.colorScheme.background),
-        ) {
-            properties.forEachIndexed { index, pair ->
                 Row(
                     modifier =
-                        Modifier.fillMaxWidth()
+                        Modifier.wrapContentSize()
                             .padding(12.dp),
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Text(
-                        modifier = Modifier.width(maxWidth + 16.dp),
-                        text = copywriter.getText(pair.first),
+                        text = "${copywriter.getText("allow_receive_from")} ${syncRuntimeInfo.getDeviceDisplayName()}",
+                        color = MaterialTheme.colorScheme.onBackground,
                         style = TextStyle(fontWeight = FontWeight.Light),
                         fontFamily = FontFamily.SansSerif,
-                        color = MaterialTheme.colorScheme.onBackground,
                         fontSize = 14.sp,
+                        modifier = Modifier.weight(1f),
                     )
-                    Text(
-                        text = pair.second,
-                        style = TextStyle(fontWeight = FontWeight.Light),
-                        fontFamily = FontFamily.SansSerif,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        fontSize = 12.sp,
+                    CustomSwitch(
+                        modifier =
+                            Modifier.align(Alignment.CenterVertically)
+                                .width(32.dp)
+                                .height(20.dp),
+                        checked = syncRuntimeInfo.allowReceive,
+                        onCheckedChange = {
+                            runBlocking {
+                                syncManager.getSyncHandlers()[syncRuntimeInfo.appInstanceId]
+                                    ?.update {
+                                        this.allowReceive = it
+                                    }?.let {
+                                        syncRuntimeInfo = it
+                                    }
+                            }
+                        },
+                        checkedThumbColor = connectedColor(),
                     )
                 }
-                if (index < properties.size - 1) {
-                    HorizontalDivider(modifier = Modifier.padding(start = 15.dp))
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            var maxWidth by remember { mutableStateOf(0.dp) }
+
+            val properties =
+                remember(syncRuntimeInfo) {
+                    arrayOf(
+                        Pair("app_version", syncRuntimeInfo.appVersion),
+                        Pair("user_name", syncRuntimeInfo.userName),
+                        Pair("device_id", syncRuntimeInfo.deviceId),
+                        Pair("arch", syncRuntimeInfo.platformArch),
+                        Pair("connect_host", syncRuntimeInfo.connectHostAddress ?: ""),
+                        Pair("port", syncRuntimeInfo.port.toString()),
+                    )
+                }
+
+            val textStyle =
+                TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal,
+                )
+
+            for (property in properties) {
+                maxWidth =
+                    maxOf(maxWidth, measureTextWidth(copywriter.getText(property.first), textStyle))
+            }
+
+            Text(
+                modifier =
+                    Modifier.wrapContentSize()
+                        .padding(start = 15.dp, bottom = 5.dp),
+                text = copywriter.getText("base_info"),
+                color = MaterialTheme.colorScheme.onBackground,
+                fontFamily = FontFamily.SansSerif,
+                style = MaterialTheme.typography.headlineSmall,
+                fontSize = 12.sp,
+            )
+
+            Column(
+                modifier =
+                    Modifier.wrapContentSize()
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(MaterialTheme.colorScheme.background),
+            ) {
+                properties.forEachIndexed { index, pair ->
+                    Row(
+                        modifier =
+                            Modifier.fillMaxWidth()
+                                .padding(12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(
+                            modifier = Modifier.width(maxWidth + 16.dp),
+                            text = copywriter.getText(pair.first),
+                            style = TextStyle(fontWeight = FontWeight.Light),
+                            fontFamily = FontFamily.SansSerif,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 14.sp,
+                        )
+                        Text(
+                            text = pair.second,
+                            style = TextStyle(fontWeight = FontWeight.Light),
+                            fontFamily = FontFamily.SansSerif,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 12.sp,
+                        )
+                    }
+                    if (index < properties.size - 1) {
+                        HorizontalDivider(modifier = Modifier.padding(start = 15.dp))
+                    }
                 }
             }
         }
