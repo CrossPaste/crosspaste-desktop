f9a69af9a744d0d2f03fa4d8bbbc0576f6a3dbd7
Yiqun Zhang <guiyanakuang@gmail.com>
2024-11-14 16:33:57 +0800

    :sparkles: Implement custom encryption protocol to replace signal

--- composeApp/src/desktopMain/kotlin/com/crosspaste/utils/EncryptUtils.kt
+++ composeApp/src/desktopMain/kotlin/com/crosspaste/utils/EncryptUtils.kt
@@ -1,15 +1,6 @@
 package com.crosspaste.utils
 
-import com.crosspaste.realm.signal.PastePreKey
-import com.crosspaste.realm.signal.PasteSignedPreKey
-import com.crosspaste.realm.signal.SignalRealm
-import org.signal.libsignal.protocol.ecc.Curve
-import org.signal.libsignal.protocol.ecc.ECPrivateKey
-import org.signal.libsignal.protocol.state.PreKeyRecord
-import org.signal.libsignal.protocol.state.SignedPreKeyRecord
-import org.signal.libsignal.protocol.util.Medium
 import java.security.SecureRandom
-import java.util.Random
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
@@ -19,46 +10,6 @@ import javax.crypto.spec.SecretKeySpec
 object EncryptUtils {
 
     private val codecsUtils = getCodecsUtils()
-
-    @Synchronized
-    fun generatePreKeyPair(signalRealm: SignalRealm): PastePreKey {
-        val preKeyPair = Curve.generateKeyPair()
-        val random = Random()
-        var preKeyId: Int
-        do {
-            preKeyId = random.nextInt(Medium.MAX_VALUE)
-        } while (signalRealm.existPreKey(preKeyId))
-        val preKeyRecord = PreKeyRecord(preKeyId, preKeyPair)
-        val serialize = preKeyRecord.serialize()
-        signalRealm.storePreKey(preKeyId, serialize)
-        return PastePreKey(preKeyId, serialize)
-    }
-
-    @Synchronized
-    fun generatesSignedPreKeyPair(
-        signalRealm: SignalRealm,
-        privateKey: ECPrivateKey,
-    ): PasteSignedPreKey {
-        val random = Random()
-        val signedPreKeyId = random.nextInt(Medium.MAX_VALUE)
-
-        signalRealm.getSignedPreKey(signedPreKeyId)?.let { signedPreKey ->
-            return signedPreKey
-        } ?: run {
-            val signedPreKeyPair = Curve.generateKeyPair()
-            val signedPreKeySignature =
-                Curve.calculateSignature(
-                    privateKey,
-                    signedPreKeyPair.publicKey.serialize(),
-                )
-            val signedPreKeyRecord =
-                SignedPreKeyRecord(
-                    signedPreKeyId, System.currentTimeMillis(), signedPreKeyPair, signedPreKeySignature,
-                )
-            signalRealm.storeSignedPreKey(signedPreKeyId, signedPreKeyRecord.serialize())
-            return PasteSignedPreKey(signedPreKeyId, signedPreKeyRecord.serialize())
-        }
-    }
 
     fun generateAESKey(): SecretKey {
         val keyGen = KeyGenerator.getInstance("AES")
