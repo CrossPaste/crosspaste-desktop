350c613d843fa8290abaabe083ffd6fbc2cda41d
Yiqun Zhang <guiyanakuang@gmail.com>
2024-11-14 21:08:13 +0800

    :zap: Optimize heartbeat performance by caching sync information to avoid repeated updates (#2189)

--- composeApp/src/commonMain/kotlin/com/crosspaste/net/SyncInfoFactory.kt
+++ composeApp/src/commonMain/kotlin/com/crosspaste/net/SyncInfoFactory.kt
@@ -3,13 +3,14 @@ package com.crosspaste.net
 import com.crosspaste.app.AppInfo
 import com.crosspaste.app.EndpointInfoFactory
 import com.crosspaste.dto.sync.SyncInfo
-import com.crosspaste.realm.sync.HostInfo
+import com.crosspaste.utils.HostInfoFilter
 
 class SyncInfoFactory(
     val appInfo: AppInfo,
     private val endpointInfoFactory: EndpointInfoFactory,
 ) {
-    fun createSyncInfo(hostInfoFilter: (HostInfo) -> Boolean): SyncInfo {
+
+    fun createSyncInfo(hostInfoFilter: HostInfoFilter): SyncInfo {
         // todo add cache, createEndpointInfo maybe slow
         return SyncInfo(
             appInfo = appInfo,
