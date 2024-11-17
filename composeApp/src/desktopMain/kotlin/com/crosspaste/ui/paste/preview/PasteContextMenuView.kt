4360d346bf1237792529e7f50e56c094942f1361
Yiqun Zhang <guiyanakuang@gmail.com>
2024-11-17 22:38:57 +0800

    :bug: Fix bugs related to multi-file and multi-image pasteboard handling

--- composeApp/src/desktopMain/kotlin/com/crosspaste/ui/paste/preview/PasteContextMenuView.kt
+++ composeApp/src/desktopMain/kotlin/com/crosspaste/ui/paste/preview/PasteContextMenuView.kt
@@ -2,25 +2,21 @@ package com.crosspaste.ui.paste.preview
 
 import androidx.compose.foundation.ContextMenuArea
 import androidx.compose.foundation.ContextMenuItem
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun PasteContextMenuView(
-    modifier: Modifier = Modifier,
     items: () -> List<ContextMenuItem>,
     content: @Composable () -> Unit,
 ) {
-    Row(modifier = modifier.fillMaxWidth()) {
+    Row(modifier = Modifier.wrapContentSize()) {
         ContextMenuArea(
             items = items,
         ) {
-            Box(modifier = Modifier.fillMaxWidth()) { // 内部内容使用 fillMaxWidth
-                content()
-            }
+            content()
         }
     }
 }
