695b307c1e9df0222d626bc36b25c6b303f55c45
Yiqun Zhang <guiyanakuang@gmail.com>
2024-11-27 21:29:17 +0800

    :hammer: Base64 implementation using kotlin.io, better multiplatform equivalent implementation (#2232)

--- composeApp/src/desktopMain/kotlin/com/crosspaste/utils/CodecsUtils.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/crosspaste/utils/CodecsUtils.desktop.kt
@@ -3,7 +3,6 @@ package com.crosspaste.utils
 import dev.whyoleg.cryptography.CryptographyProvider
 import dev.whyoleg.cryptography.algorithms.SHA256
 import java.io.ByteArrayOutputStream
-import java.util.Base64
 
 actual fun getCodecsUtils(): CodecsUtils {
     return DesktopCodecsUtils
@@ -16,14 +15,6 @@ object DesktopCodecsUtils : CodecsUtils {
     private val provider = CryptographyProvider.Default
 
     override val sha256 = provider.get(SHA256).hasher()
-
-    override fun base64Encode(bytes: ByteArray): String {
-        return Base64.getEncoder().encodeToString(bytes)
-    }
-
-    override fun base64Decode(string: String): ByteArray {
-        return Base64.getDecoder().decode(string)
-    }
 
     override fun hashByArray(array: Array<String>): String {
         if (array.isEmpty()) {
