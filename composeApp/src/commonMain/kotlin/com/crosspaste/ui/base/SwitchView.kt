d2a0e0fbb5f1805fab8470d37fd4e26e9f8aaeb2
Yiqun Zhang <guiyanakuang@gmail.com>
2024-12-21 21:26:29 +0800

    :bug: Fix switch component with label text (#2310)
    Fix text container overflow when switching languages

--- composeApp/src/commonMain/kotlin/com/crosspaste/ui/base/SwitchView.kt
+++ composeApp/src/commonMain/kotlin/com/crosspaste/ui/base/SwitchView.kt
@@ -1,26 +1,31 @@
 package com.crosspaste.ui.base
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 
 @Composable
 fun CustomSwitch(
@@ -62,7 +67,7 @@ fun CustomSwitch(
 }
 
 @Composable
-fun CustomRectangleSwitch(
+fun CustomTextSwitch(
     checked: Boolean,
     onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
@@ -70,71 +75,74 @@ fun CustomRectangleSwitch(
     checkedText: String = "ON",
     uncheckedText: String = "OFF",
 ) {
-    val checkedThumbColor = MaterialTheme.colorScheme.primary
+    val switchPadding = 4.dp
+    val thumbSize = 20.dp
 
-    val uncheckedThumbColor = MaterialTheme.colorScheme.background
+    val maxText = if (checked) checkedText else uncheckedText
+    val maxTextWidth = measureTextWidth(maxText, textStyle)
 
-    val maxWidth = max(measureTextWidth(checkedText, textStyle), measureTextWidth(uncheckedText, textStyle))
-
-    Box(modifier = modifier) {
-        Canvas(
+    Box(
+        modifier =
+            modifier
+                .height(28.dp),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        Box(
             modifier =
-                modifier
-                    .clickable {
-                        onCheckedChange(!checked)
-                    }
-                    .padding(horizontal = 4.dp),
+                Modifier
+                    .width(maxTextWidth + thumbSize + switchPadding * 4)
+                    .height(28.dp)
+                    .clip(RoundedCornerShape(14.dp))
+                    .background(MaterialTheme.colorScheme.primary)
+                    .clickable { onCheckedChange(!checked) },
+            contentAlignment = Alignment.Center,
         ) {
-            val trackHeight = size.height
-            val trackWidth = size.width
-            val cornerRadius = CornerRadius(x = 4.dp.toPx(), y = 4.dp.toPx())
-
-            val thumbWidth = size.width - maxWidth.toPx() - 12.dp.toPx()
-
-            // Draw the background track
-            drawRoundRect(
-                color = checkedThumbColor,
-                size = Size(width = trackWidth, height = trackHeight),
-                cornerRadius = cornerRadius,
-            )
-
-            val knobOffset = if (checked) maxWidth.toPx() + 8.dp.toPx() else 4.dp.toPx()
-
-            // Draw the knob
-            drawRoundRect(
-                topLeft = Offset(knobOffset, 2.dp.toPx()),
-                color = uncheckedThumbColor,
-                size = Size(width = thumbWidth, height = trackHeight - 4.dp.toPx()),
-                cornerRadius = cornerRadius,
-            )
-        }
-
-        if (checked) {
             Row(
-                modifier = modifier,
-                horizontalArrangement = Arrangement.Start,
+                horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Spacer(modifier = Modifier.padding(4.dp))
-                Text(
-                    text = checkedText,
-                    style = textStyle,
-                    color = Color.White,
+                Spacer(
+                    modifier =
+                        Modifier.width(
+                            if (!checked) {
+                                switchPadding * 2
+                            } else {
+                                (thumbSize + switchPadding * 2)
+                            },
+                        ),
+                )
+                Row(
+                    modifier = Modifier.width(maxTextWidth),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = if (checked) checkedText else uncheckedText,
+                        color = Color.White,
+                        style = textStyle,
+                        maxLines = 1,
+                    )
+                }
+                Spacer(
+                    modifier =
+                        Modifier.width(
+                            if (checked) {
+                                switchPadding * 2
+                            } else {
+                                (thumbSize + switchPadding * 2)
+                            },
+                        ),
                 )
             }
-        } else {
-            Row(
-                modifier = modifier,
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = uncheckedText,
-                    style = textStyle,
-                    color = Color.White,
-                )
-                Spacer(modifier = Modifier.padding(4.dp))
-            }
+            Box(
+                modifier =
+                    Modifier
+                        .padding(switchPadding)
+                        .size(thumbSize)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .align(if (checked) Alignment.CenterStart else Alignment.CenterEnd),
+            )
         }
     }
 }
