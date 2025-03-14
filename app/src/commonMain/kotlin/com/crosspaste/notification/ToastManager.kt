f657f3a0856fbe9f1650ddc878c1dde4608bc409
Yiqun Zhang <guiyanakuang@gmail.com>
2025-03-14 16:06:23 +0800

    :bug: Fix toast animation issue on Android platform (#2497)

--- app/src/commonMain/kotlin/com/crosspaste/notification/ToastManager.kt
+++ app/src/commonMain/kotlin/com/crosspaste/notification/ToastManager.kt
@@ -1,15 +1,9 @@
 package com.crosspaste.notification
 
-import com.crosspaste.utils.mainDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class ToastManager {
-
-    private val coroutineScope = CoroutineScope(mainDispatcher)
 
     private val _toastList: MutableStateFlow<List<Message>> = MutableStateFlow(listOf())
 
@@ -17,13 +11,6 @@ class ToastManager {
 
     fun pushToast(toast: Message) {
         this._toastList.value = listOf(toast) + this._toastList.value
-
-        toast.duration?.let { duration ->
-            coroutineScope.launch {
-                delay(duration)
-                removeToast(toast.messageId)
-            }
-        }
     }
 
     fun removeToast(messageId: Int) {
