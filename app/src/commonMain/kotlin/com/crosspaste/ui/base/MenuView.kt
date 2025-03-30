0128608401c078f57d9325dc47bd25037fa79591
Yiqun Zhang <guiyanakuang@gmail.com>
2025-03-30 12:44:12 +0800

    :lipstick: Enhanced date picker UI

--- app/src/commonMain/kotlin/com/crosspaste/ui/base/MenuView.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/base/MenuView.kt
@@ -8,13 +8,10 @@ import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
@@ -23,7 +20,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -56,7 +52,7 @@ fun MenuItem(
     background: Color = MaterialTheme.colorScheme.surface,
     paddingValues: PaddingValues = PaddingValues(16.dp, 8.dp, 16.dp, 8.dp),
     enabledInteraction: Boolean = true,
-    reminder: Boolean = false,
+    extendContent: (@Composable RowScope.() -> Unit)? = null,
     onClick: () -> Unit,
 ) {
     val interactionSource = remember { MutableInteractionSource() }
@@ -89,23 +85,9 @@ fun MenuItem(
             color = MaterialTheme.colorScheme.contentColorFor(backgroundColor),
             style = textStyle,
         )
-        if (reminder) {
-            Spacer(modifier = Modifier.weight(1f))
-            Row(
-                modifier =
-                    Modifier.width(32.dp)
-                        .height(16.dp)
-                        .clip(RoundedCornerShape(4.dp))
-                        .background(Color.Red),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "new!",
-                    color = Color.White,
-                    style = menuItemReminderTextStyle,
-                )
-            }
+
+        extendContent?.let {
+            it()
         }
     }
 }
--- app/src/commonMain/kotlin/com/crosspaste/ui/settings/CleanTimeMenuView.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/settings/CleanTimeMenuView.kt
@@ -3,10 +3,15 @@ package com.crosspaste.ui.settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,8 +21,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.crosspaste.clean.CleanTime
 import com.crosspaste.i18n.GlobalCopywriter
+import com.crosspaste.ui.base.BaseColor
 import com.crosspaste.ui.base.MenuItem
 import com.crosspaste.ui.base.getMenWidth
+import com.crosspaste.utils.ColorUtils
 import org.koin.compose.koinInject
 
 @Composable
@@ -39,24 +46,41 @@ fun CleanTimeMenuView(
                 "${cleanTime.quantity} ${copywriter.getText(cleanTime.unit)}"
             }.toTypedArray()
 
-        val maxWidth = getMenWidth(cleanTimeMenuTexts)
+        val maxWidth =
+            getMenWidth(cleanTimeMenuTexts) {
+                28.dp
+            }
 
         Column(
             modifier =
                 Modifier
-                    .width(maxWidth + 20.dp)
+                    .width(maxWidth)
                     .wrapContentHeight()
                     .clip(RoundedCornerShape(5.dp))
                     .background(MaterialTheme.colorScheme.surface),
         ) {
             cleanTimeMenuTexts.forEachIndexed { index, text ->
-                val menuText =
-                    if (index == selectIndex) {
-                        "$text ✔️"
-                    } else {
-                        text
-                    }
-                MenuItem(menuText) {
+                MenuItem(
+                    text = text,
+                    extendContent =
+                        if (index == selectIndex) {
+                            {
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Icon(
+                                    modifier = Modifier.size(20.dp),
+                                    imageVector = Icons.Outlined.Check,
+                                    contentDescription = "selected",
+                                    tint =
+                                        ColorUtils.getAdaptiveColor(
+                                            backgroundColor = MaterialTheme.colorScheme.surface,
+                                            BaseColor.Green.targetHue,
+                                        ),
+                                )
+                            }
+                        } else {
+                            null
+                        },
+                ) {
                     closeMenu(index)
                 }
             }
