546da3b338b09947f0adf182a350ef419cb605e9
Yiqun Zhang <guiyanakuang@gmail.com>
2025-04-30 18:02:36 +0800

    :bug: Fix the issue of incomplete Windows right-click menu display

--- app/src/commonMain/kotlin/com/crosspaste/ui/base/MenuView.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/base/MenuView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
@@ -37,7 +38,7 @@ fun MenuItem(
             lineHeight = TextUnit.Unspecified,
         ),
     background: Color = MaterialTheme.colorScheme.surface,
-    paddingValues: PaddingValues = PaddingValues(16.dp, 8.dp, 16.dp, 8.dp),
+    paddingValues: PaddingValues = PaddingValues(16.dp, 0.dp, 16.dp, 0.dp),
     enabledInteraction: Boolean = true,
     extendContent: (@Composable RowScope.() -> Unit)? = null,
     onClick: () -> Unit,
@@ -63,7 +64,9 @@ fun MenuItem(
     }
 
     Row(
-        modifier = modifier.padding(paddingValues),
+        modifier =
+            modifier.height(32.dp)
+                .padding(paddingValues),
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically,
     ) {
