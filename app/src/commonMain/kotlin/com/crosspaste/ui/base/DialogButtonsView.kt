ccd48996f1fe2eb14a131d4d4e6e3fbe68f82cd9
Yiqun Zhang <guiyanakuang@gmail.com>
2025-05-07 16:36:11 +0800

    :lipstick: Add vertical alignment to DialogButtonsView button row (#2713)

--- app/src/commonMain/kotlin/com/crosspaste/ui/base/DialogButtonsView.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/base/DialogButtonsView.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -31,6 +32,7 @@ fun DialogButtonsView(
                 .fillMaxWidth()
                 .wrapContentHeight(),
         horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         OutlinedButton(
             onClick = cancelAction,
