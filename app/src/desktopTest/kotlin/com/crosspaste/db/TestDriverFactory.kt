4406b8054e298d9f1cd81454301c63007c11a3cb
Yiqun Zhang <guiyanakuang@gmail.com>
2025-05-21 18:04:33 +0800

    :white_check_mark: Add unit tests for SyncRuntimeInfoDao reactive flow behavior (#2766)

--- app/src/desktopTest/kotlin/com/crosspaste/db/TestDriverFactory.kt
+++ app/src/desktopTest/kotlin/com/crosspaste/db/TestDriverFactory.kt
@@ -2,6 +2,7 @@ package com.crosspaste.db
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import com.crosspaste.Database
 
 class TestDriverFactory: DriverFactory {
     override val dbName: String = "crosspaste_test.db"
@@ -9,6 +10,7 @@ class TestDriverFactory: DriverFactory {
 
     override fun createDriver(): SqlDriver {
         sqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        Database.Schema.create(sqlDriver!!)
         return sqlDriver!!
     }
 
