08b4c83486c555068480fd58728c8c77450cec4d
Yiqun Zhang <guiyanakuang@gmail.com>
2025-06-04 00:12:07 +0800

    :sparkles: Auto-generate missing text clipboard from HTML/RTF (#2828)

---
+++ app/src/commonMain/kotlin/com/crosspaste/paste/plugin/process/GenerateTextPlugin.kt
@@ -0,0 +1,39 @@
+package com.crosspaste.paste.plugin.process
+
+import com.crosspaste.paste.item.HtmlPasteItem
+import com.crosspaste.paste.item.PasteItem
+import com.crosspaste.paste.item.RtfPasteItem
+import com.crosspaste.paste.item.TextPasteItem
+
+object GenerateTextPlugin : PasteProcessPlugin {
+    override fun process(
+        pasteItems: List<PasteItem>,
+        source: String?,
+    ): List<PasteItem> {
+        if (pasteItems.any { it is HtmlPasteItem } &&
+            pasteItems.all { it !is TextPasteItem }
+        ) {
+            pasteItems.filterIsInstance<HtmlPasteItem>().firstOrNull()?.let {
+                val text = it.getText()
+                return pasteItems +
+                    TextPasteItem.createTextPasteItem(
+                        text = text,
+                    )
+            }
+        }
+
+        if (pasteItems.any { it is RtfPasteItem } &&
+            pasteItems.all { it !is TextPasteItem }
+        ) {
+            pasteItems.filterIsInstance<RtfPasteItem>().firstOrNull()?.let {
+                val text = it.getText()
+                return pasteItems +
+                    TextPasteItem.createTextPasteItem(
+                        text = text,
+                    )
+            }
+        }
+
+        return pasteItems
+    }
+}
