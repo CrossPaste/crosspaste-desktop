CrossPaste/crosspaste-desktop

commit bb62b9ed595df8c27b8102027bffd6ddbc50ccb5

:bug: Create custom serializers for HtmlPasteItem and RtfPasteItem for legacy version compatibility (#2903)

app/src/commonMain/kotlin/com/crosspaste/paste/item/HtmlPasteItem.kt
@@ -6,10 +6,10 @@ import com.crosspaste.db.paste.PasteType
 import com.crosspaste.paste.item.PasteItem.Companion.getExtraInfoFromJson
 import com.crosspaste.paste.item.PasteItemProperties.BACKGROUND
 import com.crosspaste.path.UserDataPathProvider
+import com.crosspaste.serializer.HtmlPasteItemSerializer
 import com.crosspaste.utils.getFileUtils
 import com.crosspaste.utils.getHtmlUtils
 import com.crosspaste.utils.getJsonUtils
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
@@ -19,13 +19,12 @@ import kotlinx.serialization.json.put
 import okio.Path
 import okio.Path.Companion.toPath
 
-@Serializable
-@SerialName("html")
+@Serializable(with = HtmlPasteItemSerializer::class)
 class HtmlPasteItem(
     override val identifiers: List<String>,
     override val hash: String,
-    override val size: Long,
     override val html: String,
+    override val size: Long,
     override val extraInfo: JsonObject? = null,
 ) : PasteItem, PasteHtml {
 

app/src/commonMain/kotlin/com/crosspaste/paste/item/RtfPasteItem.kt
@@ -22,8 +22,8 @@ import okio.Path.Companion.toPath
 class RtfPasteItem(
     override val identifiers: List<String>,
     override val hash: String,
-    override val size: Long,
     override val rtf: String,
+    override val size: Long,
     override val extraInfo: JsonObject? = null,
 ) : PasteItem, PasteRtf {
 

app/src/commonMain/kotlin/com/crosspaste/serializer/HtmlPasteItemSerializer.kt
@@ -1,0 +1,76 @@
+package com.crosspaste.serializer
+
+import com.crosspaste.paste.item.HtmlPasteItem
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonObject
+
+class HtmlPasteItemSerializer : KSerializer<HtmlPasteItem> {
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("HtmlPasteItem") {
+            element<List<String>>("identifiers")
+            element<String>("hash")
+            element<String>("html")
+            element<Long>("size")
+            element<JsonObject?>("extraInfo")
+            element<String>("relativePath")
+        }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun deserialize(decoder: Decoder): HtmlPasteItem {
+        val dec = decoder.beginStructure(descriptor)
+        var identifiers: List<String> = listOf()
+        var hash = ""
+        var html = ""
+        var size = 0L
+        var extraInfo: JsonObject? = null
+
+        loop@ while (true) {
+            when (dec.decodeElementIndex(descriptor)) {
+                0 -> identifiers = dec.decodeSerializableElement(descriptor, 0, ListSerializer(String.serializer()))
+                1 -> hash = dec.decodeStringElement(descriptor, 1)
+                2 -> html = dec.decodeStringElement(descriptor, 2)
+                3 -> size = dec.decodeLongElement(descriptor, 3)
+                4 -> extraInfo = dec.decodeNullableSerializableElement(descriptor, 4, JsonObject.serializer())
+                CompositeDecoder.DECODE_DONE -> break@loop
+                else -> {
+                    dec.decodeElementIndex(descriptor)
+                }
+            }
+        }
+
+        dec.endStructure(descriptor)
+
+        return HtmlPasteItem(
+            identifiers = identifiers,
+            hash = hash,
+            html = html,
+            size = size,
+            extraInfo = extraInfo,
+        )
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun serialize(
+        encoder: Encoder,
+        value: HtmlPasteItem,
+    ) {
+        val enc = encoder.beginStructure(descriptor)
+        enc.encodeSerializableElement(descriptor, 0, ListSerializer(String.serializer()), value.identifiers)
+        enc.encodeStringElement(descriptor, 1, value.hash)
+        enc.encodeStringElement(descriptor, 2, value.html)
+        enc.encodeLongElement(descriptor, 3, value.size)
+        enc.encodeNullableSerializableElement(descriptor, 4, JsonObject.serializer(), value.extraInfo)
+        // To be compatible with older versions, we must set this field
+        enc.encodeStringElement(descriptor, 5, "")
+        enc.endStructure(descriptor)
+    }
+}

app/src/commonMain/kotlin/com/crosspaste/serializer/RtfPasteItemSerializer.kt
@@ -1,0 +1,76 @@
+package com.crosspaste.serializer
+
+import com.crosspaste.paste.item.RtfPasteItem
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonObject
+
+class RtfPasteItemSerializer : KSerializer<RtfPasteItem> {
+    override val descriptor: SerialDescriptor =
+        buildClassSerialDescriptor("RtfPasteItem") {
+            element<List<String>>("identifiers")
+            element<String>("hash")
+            element<String>("rtf")
+            element<Long>("size")
+            element<JsonObject?>("extraInfo")
+            element<String>("relativePath")
+        }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun deserialize(decoder: Decoder): RtfPasteItem {
+        val dec = decoder.beginStructure(descriptor)
+        var identifiers: List<String> = listOf()
+        var hash = ""
+        var rtf = ""
+        var size = 0L
+        var extraInfo: JsonObject? = null
+
+        loop@ while (true) {
+            when (dec.decodeElementIndex(descriptor)) {
+                0 -> identifiers = dec.decodeSerializableElement(descriptor, 0, ListSerializer(String.serializer()))
+                1 -> hash = dec.decodeStringElement(descriptor, 1)
+                2 -> rtf = dec.decodeStringElement(descriptor, 2)
+                3 -> size = dec.decodeLongElement(descriptor, 3)
+                4 -> extraInfo = dec.decodeNullableSerializableElement(descriptor, 4, JsonObject.serializer())
+                CompositeDecoder.DECODE_DONE -> break@loop
+                else -> {
+                    dec.decodeElementIndex(descriptor)
+                }
+            }
+        }
+
+        dec.endStructure(descriptor)
+
+        return RtfPasteItem(
+            identifiers = identifiers,
+            hash = hash,
+            rtf = rtf,
+            size = size,
+            extraInfo = extraInfo,
+        )
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun serialize(
+        encoder: Encoder,
+        value: RtfPasteItem,
+    ) {
+        val enc = encoder.beginStructure(descriptor)
+        enc.encodeSerializableElement(descriptor, 0, ListSerializer(String.serializer()), value.identifiers)
+        enc.encodeStringElement(descriptor, 1, value.hash)
+        enc.encodeStringElement(descriptor, 2, value.rtf)
+        enc.encodeLongElement(descriptor, 3, value.size)
+        enc.encodeNullableSerializableElement(descriptor, 4, JsonObject.serializer(), value.extraInfo)
+        // To be compatible with older versions, we must set this field
+        enc.encodeStringElement(descriptor, 5, "")
+        enc.endStructure(descriptor)
+    }
+}

app/src/desktopMain/kotlin/com/crosspaste/utils/JsonUtils.desktop.kt
@@ -16,6 +16,8 @@ import com.crosspaste.presist.DirFileInfoTree
 import com.crosspaste.presist.FileInfoTree
 import com.crosspaste.presist.SingleFileInfoTree
 import com.crosspaste.serializer.Base64ByteArraySerializer
+import com.crosspaste.serializer.HtmlPasteItemSerializer
+import com.crosspaste.serializer.RtfPasteItemSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
@@ -41,9 +43,9 @@ object DesktopJsonUtils : JsonUtils {
                     polymorphic(PasteItem::class) {
                         subclass(ColorPasteItem::class)
                         subclass(FilesPasteItem::class)
-                        subclass(HtmlPasteItem::class)
+                        subclass(HtmlPasteItem::class, HtmlPasteItemSerializer())
                         subclass(ImagesPasteItem::class)
-                        subclass(RtfPasteItem::class)
+                        subclass(RtfPasteItem::class, RtfPasteItemSerializer())
                         subclass(TextPasteItem::class)
                         subclass(UrlPasteItem::class)
                     }

commit 6617ea98d19155a2f64a413e2599f8048540a91b

:bug: Resolve pasteboard JSON serialization bug
- Fix regression introduced in #2903 and #2904

app/src/commonMain/kotlin/com/crosspaste/paste/item/HtmlPasteItem.kt
@@ -10,6 +10,7 @@ import com.crosspaste.serializer.HtmlPasteItemSerializer
 import com.crosspaste.utils.getFileUtils
 import com.crosspaste.utils.getHtmlUtils
 import com.crosspaste.utils.getJsonUtils
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
@@ -20,6 +21,7 @@ import okio.Path
 import okio.Path.Companion.toPath
 
 @Serializable(with = HtmlPasteItemSerializer::class)
+@SerialName("html")
 class HtmlPasteItem(
     override val identifiers: List<String>,
     override val hash: String,

app/src/commonMain/kotlin/com/crosspaste/paste/item/RtfPasteItem.kt
@@ -4,6 +4,7 @@ import com.crosspaste.app.AppFileType
 import com.crosspaste.db.paste.PasteType
 import com.crosspaste.paste.item.PasteItem.Companion.getExtraInfoFromJson
 import com.crosspaste.path.UserDataPathProvider
+import com.crosspaste.serializer.RtfPasteItemSerializer
 import com.crosspaste.utils.getFileUtils
 import com.crosspaste.utils.getJsonUtils
 import com.crosspaste.utils.getRtfUtils
@@ -17,7 +18,7 @@ import kotlinx.serialization.json.put
 import okio.Path
 import okio.Path.Companion.toPath
 
-@Serializable
+@Serializable(with = RtfPasteItemSerializer::class)
 @SerialName("rtf")
 class RtfPasteItem(
     override val identifiers: List<String>,