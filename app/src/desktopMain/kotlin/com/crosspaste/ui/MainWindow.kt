4419cb0b64bdafd6f32a8fbe5ec257adabd1f8e9
Yiqun Zhang <guiyanakuang@gmail.com>
2025-07-25 19:49:16 +0800

    :lipstick: Support native app menu

--- app/src/desktopMain/kotlin/com/crosspaste/ui/MainWindow.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/ui/MainWindow.kt
@@ -13,11 +13,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.window.MenuBar
 import com.crosspaste.app.DesktopAppSize
 import com.crosspaste.app.DesktopAppWindowManager
+import com.crosspaste.i18n.GlobalCopywriter
 import com.crosspaste.listener.GlobalListener
 import com.crosspaste.ui.base.PasteTooltipIconView
 import com.crosspaste.ui.base.pushpinActive
@@ -30,6 +35,7 @@ import org.koin.compose.koinInject
 fun MainWindow(windowIcon: Painter?) {
     val appSize = koinInject<DesktopAppSize>()
     val appWindowManager = koinInject<DesktopAppWindowManager>()
+    val copywriter = koinInject<GlobalCopywriter>()
     val globalListener = koinInject<GlobalListener>()
 
     val alwaysOnTop by appWindowManager.alwaysOnTopMainWindow.collectAsState()
@@ -86,6 +92,35 @@ fun MainWindow(windowIcon: Painter?) {
                         },
                     )
                 }
+            }
+        }
+
+        var action by remember { mutableStateOf("Last action: None") }
+        var isOpen by remember { mutableStateOf(true) }
+
+        var isSubmenuShowing by remember { mutableStateOf(false) }
+
+        MenuBar {
+            Menu("sync") {
+                Item("devices", onClick = { })
+                Item("Paste", onClick = { action = "Last action: Paste" }, shortcut = KeyShortcut(Key.V, ctrl = true))
+            }
+            Menu("Actions", mnemonic = 'A') {
+                CheckboxItem(
+                    "Advanced settings",
+                    checked = isSubmenuShowing,
+                    onCheckedChange = {
+                        isSubmenuShowing = !isSubmenuShowing
+                    },
+                )
+                if (isSubmenuShowing) {
+                    Menu("Settings") {
+                        Item("Setting 1", onClick = { action = "Last action: Setting 1" })
+                        Item("Setting 2", onClick = { action = "Last action: Setting 2" })
+                    }
+                }
+                Separator()
+                Item("Exit", onClick = { isOpen = false }, shortcut = KeyShortcut(Key.Escape), mnemonic = 'E')
             }
         }
 
