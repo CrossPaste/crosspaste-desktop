7e0d1ee23e535648712290d82807503302c06836
Yiqun Zhang <guiyanakuang@gmail.com>
2025-08-18 15:34:42 +0800

    :hammer: Make HighlightedCard component more flexible
    - Replace containerColor parameter with CardColors for better customization
    - Make elevation configurable with default value
    - Make border configurable with default value
    - Improve component reusability by allowing full control over card appearance

--- app/src/commonMain/kotlin/com/crosspaste/ui/base/HighlightedCard.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/base/HighlightedCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,7 +16,7 @@ import com.crosspaste.ui.theme.AppUISize.tiny6X
 fun HighlightedCard(
     modifier: Modifier,
     shape: RoundedCornerShape,
-    colors: CardColors,
+    colors: CardColors = CardDefaults.cardColors(),
     elevation: CardElevation = highlightedCardElevation,
     border: BorderStroke =
         BorderStroke(
