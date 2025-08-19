ab57dfa0ccca78fbe72b96d525cdfb837b8c2b46
Yiqun Zhang <guiyanakuang@gmail.com>
2025-08-19 18:47:31 +0800

    :bug: Fix PasteDataScopeImpl implementation (#3132)
    1. Remove javaClass to support multi-platform
     2. Remove internal modifier to enable reuse on mobile platforms

--- app/src/commonMain/kotlin/com/crosspaste/ui/paste/PasteDataScope.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/paste/PasteDataScope.kt
@@ -18,15 +18,14 @@ fun createPasteDataScope(pasteData: PasteData): PasteDataScope? =
         null
     }
 
-internal class PasteDataScopeImpl(
+class PasteDataScopeImpl(
     override val pasteData: PasteData,
 ) : PasteDataScope {
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
-
-        other as PasteDataScopeImpl
+        if (other !is PasteDataScopeImpl) return false
 
         return pasteData == other.pasteData
     }
