d07dcf143b555137e2b9ba467441dc4df7b99925
Yiqun Zhang <guiyanakuang@gmail.com>
2025-09-09 13:32:24 +0800

    :hammer: Update theme definitions to use non-deprecated JewelTheme API

--- app/src/desktopMain/kotlin/com/crosspaste/ui/theme/DesktopTheme.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/ui/theme/DesktopTheme.kt
@@ -3,10 +3,20 @@ package com.crosspaste.ui.theme
 import androidx.compose.runtime.Composable
 import com.crosspaste.app.DesktopAppSize
 import com.crosspaste.ui.theme.CrossPasteTheme.Theme
+import org.jetbrains.jewel.foundation.DisabledAppearanceValues
+import org.jetbrains.jewel.foundation.GlobalColors
+import org.jetbrains.jewel.foundation.GlobalMetrics
 import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.intui.core.theme.IntUiDarkTheme
+import org.jetbrains.jewel.intui.core.theme.IntUiLightTheme
 import org.jetbrains.jewel.intui.standalone.theme.IntUiTheme
+import org.jetbrains.jewel.intui.standalone.theme.createDefaultTextStyle
+import org.jetbrains.jewel.intui.standalone.theme.createEditorTextStyle
+import org.jetbrains.jewel.intui.standalone.theme.dark
 import org.jetbrains.jewel.intui.standalone.theme.darkThemeDefinition
 import org.jetbrains.jewel.intui.standalone.theme.default
+import org.jetbrains.jewel.intui.standalone.theme.defaults
+import org.jetbrains.jewel.intui.standalone.theme.light
 import org.jetbrains.jewel.intui.standalone.theme.lightThemeDefinition
 import org.jetbrains.jewel.intui.window.decoratedWindow
 import org.jetbrains.jewel.intui.window.styling.dark
@@ -27,9 +37,29 @@ fun DesktopTheme(content: @Composable () -> Unit) {
         IntUiTheme(
             theme =
                 if (themeDetector.isCurrentThemeDark()) {
-                    JewelTheme.darkThemeDefinition()
+                    JewelTheme.darkThemeDefinition(
+                        colors = GlobalColors.dark(),
+                        metrics = GlobalMetrics.defaults(),
+                        palette = IntUiDarkTheme.colors,
+                        iconData = IntUiDarkTheme.iconData,
+                        defaultTextStyle = JewelTheme.createDefaultTextStyle(),
+                        editorTextStyle = JewelTheme.createEditorTextStyle(),
+                        consoleTextStyle = JewelTheme.createEditorTextStyle(),
+                        contentColor = GlobalColors.dark().text.normal,
+                        disabledAppearanceValues = DisabledAppearanceValues.dark(),
+                    )
                 } else {
-                    JewelTheme.lightThemeDefinition()
+                    JewelTheme.lightThemeDefinition(
+                        colors = GlobalColors.light(),
+                        metrics = GlobalMetrics.defaults(),
+                        palette = IntUiLightTheme.colors,
+                        iconData = IntUiLightTheme.iconData,
+                        defaultTextStyle = JewelTheme.createDefaultTextStyle(),
+                        editorTextStyle = JewelTheme.createEditorTextStyle(),
+                        consoleTextStyle = JewelTheme.createEditorTextStyle(),
+                        contentColor = GlobalColors.light().text.normal,
+                        disabledAppearanceValues = DisabledAppearanceValues.light(),
+                    )
                 },
             styling =
                 ComponentStyling.default().decoratedWindow(
