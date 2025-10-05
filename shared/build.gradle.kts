e4e03f03e69176d2d15f9de020ee3205811bd7e1
Yiqun Zhang <guiyanakuang@gmail.com>
2025-10-05 21:31:05 +0800

    :bug: Rename native target to nativeApp to avoid conflict with default hierarchy template (#3264)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -35,11 +35,11 @@ kotlin {
     val isMingwX64 = hostOs.startsWith("Windows")
 
     when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("nativeApp")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("nativeApp")
+        hostOs == "Linux" && isArm64 -> linuxArm64("nativeApp")
+        hostOs == "Linux" && !isArm64 -> linuxX64("nativeApp")
+        isMingwX64 -> mingwX64("nativeApp")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
