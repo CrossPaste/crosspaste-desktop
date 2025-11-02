d1214b110f54a61dcc60eccb1eb5c74e74d10a6d
Yiqun Zhang <guiyanakuang@gmail.com>
2025-11-02 20:49:42 +0800

    :sparkles: Add download module to prepare for future extension loading (#3296)

--- app/src/commonMain/kotlin/com/crosspaste/net/ResourcesClient.kt
+++ app/src/commonMain/kotlin/com/crosspaste/net/ResourcesClient.kt
@@ -2,12 +2,20 @@ package com.crosspaste.net
 
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsChannel
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentLength
 import io.ktor.utils.io.*
+import okio.Path
 
 interface ResourcesClient {
 
     suspend fun request(url: String): Result<ClientResponse>
+
+    suspend fun download(
+        url: String,
+        path: Path,
+        listener: DownloadProgressListener,
+    )
 }
 
 class ClientResponse(
@@ -17,4 +25,19 @@ class ClientResponse(
     suspend fun getBody(): ByteReadChannel = response.bodyAsChannel()
 
     fun getContentLength(): Long = response.contentLength() ?: -1L
+}
+
+interface DownloadProgressListener {
+
+    fun onFailure(
+        httpStatusCode: HttpStatusCode,
+        throwable: Throwable?,
+    )
+
+    fun onSuccess()
+
+    fun onProgress(
+        bytesRead: Long,
+        contentLength: Long?,
+    )
 }
--- app/src/desktopMain/kotlin/com/crosspaste/net/DesktopResourcesClient.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/net/DesktopResourcesClient.kt
@@ -1,24 +1,35 @@
 package com.crosspaste.net
 
+import com.crosspaste.app.AppFileType
 import com.crosspaste.config.DesktopConfigManager
+import com.crosspaste.path.UserDataPathProvider
 import com.crosspaste.ui.extension.ProxyType
+import com.crosspaste.utils.getFileUtils
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.ProxyBuilder
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.http
+import io.ktor.client.plugins.onDownload
 import io.ktor.client.plugins.timeout
+import io.ktor.client.request.prepareRequest
 import io.ktor.client.request.request
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsChannel
 import io.ktor.http.headers
 import io.ktor.http.isSuccess
 import io.ktor.util.collections.*
+import okio.Path
 
 class DesktopResourcesClient(
     val configManager: DesktopConfigManager,
+    val userDataPathProvider: UserDataPathProvider,
 ) : ResourcesClient {
 
     companion object {
+
+        val fileUtils = getFileUtils()
+
         fun createClient(proxyConfig: Proxy?): HttpClient =
             HttpClient(CIO) {
                 engine {
@@ -60,13 +71,57 @@ class DesktopResourcesClient(
         } ?: noProxyClient
     }
 
+    private fun getTempFilePath(): Path =
+        userDataPathProvider.resolve(
+            fileName = fileUtils.createRandomFileName(),
+            appFileType = AppFileType.TEMP,
+        )
+
+    override suspend fun download(
+        url: String,
+        path: Path,
+        listener: DownloadProgressListener,
+    ) {
+        var shouldShowProgress = false
+        getHttpClient()
+            .prepareRequest(url) {
+                onDownload { bytesSent, contentLength ->
+                    if (shouldShowProgress) {
+                        listener.onProgress(bytesSent, contentLength)
+                    }
+                }
+            }.execute { response ->
+                if (response.status.isSuccess()) {
+                    shouldShowProgress = true
+                    val tempFilePath = getTempFilePath()
+                    runCatching {
+                        val channel = response.bodyAsChannel()
+                        fileUtils.writeFile(tempFilePath, channel)
+                        fileUtils.moveFile(tempFilePath, path)
+                    }.onSuccess {
+                        listener.onSuccess()
+                    }.onFailure {
+                        if (fileUtils.existFile(tempFilePath)) {
+                            fileUtils.deleteFile(tempFilePath)
+                        }
+                        if (fileUtils.existFile(path)) {
+                            fileUtils.deleteFile(path)
+                        }
+                        listener.onFailure(response.status, it)
+                    }
+                } else {
+                    listener.onFailure(response.status, null)
+                }
+            }
+    }
+
     override suspend fun request(url: String): Result<ClientResponse> {
         val response = clientRequest(url, getHttpClient())
         return if (response.status.isSuccess()) {
             Result.success(ClientResponse(response))
         } else {
             logger.warn { "Failed to fetch data from $url, status code: ${response.status.value}" }
-            Result.failure(Exception("HTTP error: ${response.status.value}"))
+            Result.failure(kotlin.Exception("HTTP error: ${response.status.value}"))
         }
     }
 
