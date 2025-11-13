b178f86032c5bd794711b403eb6e604ce13be9da
Yiqun Zhang <guiyanakuang@gmail.com>
2025-11-13 16:45:12 +0800

    :bug: Bring main window to front when displaying token (#3311)

--- app/src/desktopMain/kotlin/com/crosspaste/app/DesktopAppTokenService.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/app/DesktopAppTokenService.kt
@@ -1,10 +1,15 @@
 package com.crosspaste.app
 
+import com.crosspaste.utils.GlobalCoroutineScope.mainCoroutineDispatcher
+import kotlinx.coroutines.launch
+
 class DesktopAppTokenService(
     private val appWindowManager: DesktopAppWindowManager,
 ) : AppTokenService() {
 
     override fun preShowToken() {
-        appWindowManager.showMainWindow()
+        mainCoroutineDispatcher.launch {
+            appWindowManager.showMainWindow()
+        }
     }
 }
