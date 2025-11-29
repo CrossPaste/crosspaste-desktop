12a5384370530e7962e22a155f85db6a97ecc35b
Yiqun Zhang <guiyanakuang@gmail.com>
2025-11-29 20:55:41 +0800

    :sparkles: Add support for automatic window focus change monitoring on Windows

--- app/src/desktopMain/kotlin/com/crosspaste/platform/windows/WindowFocusRecorder.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/platform/windows/WindowFocusRecorder.kt
@@ -3,11 +3,15 @@ package com.crosspaste.platform.windows
 import com.crosspaste.app.DesktopAppWindowManager
 import com.crosspaste.platform.windows.api.User32
 import com.crosspaste.platform.windows.api.User32.Companion.INSTANCE
+import com.crosspaste.utils.ioDispatcher
 import com.sun.jna.Native
 import com.sun.jna.platform.win32.WinDef
 import com.sun.jna.platform.win32.WinDef.HWND
 import com.sun.jna.platform.win32.WinNT
 import com.sun.jna.platform.win32.WinUser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class WindowFocusRecorder(
     private val appWindowManager: DesktopAppWindowManager,
@@ -18,36 +22,46 @@ class WindowFocusRecorder(
 
     private var lastActiveWindow: HWND? = null
 
+    private val scope = CoroutineScope(ioDispatcher + SupervisorJob())
+
     fun start() {
         if (hookHandle != null) return
 
-        eventProc =
-            object : WinUser.WinEventProc {
-                override fun callback(
-                    hWinEventHook: WinNT.HANDLE?,
-                    event: WinDef.DWORD?,
-                    hwnd: HWND?,
-                    idObject: WinDef.LONG?,
-                    idChild: WinDef.LONG?,
-                    dwEventThread: WinDef.DWORD?,
-                    dwmsEventTime: WinDef.DWORD?,
-                ) {
-                    if (hwnd != null && isValidWindow(hwnd)) {
-                        lastActiveWindow = hwnd
+        scope.launch {
+            eventProc =
+                object : WinUser.WinEventProc {
+                    override fun callback(
+                        hWinEventHook: WinNT.HANDLE?,
+                        event: WinDef.DWORD?,
+                        hwnd: HWND?,
+                        idObject: WinDef.LONG?,
+                        idChild: WinDef.LONG?,
+                        dwEventThread: WinDef.DWORD?,
+                        dwmsEventTime: WinDef.DWORD?,
+                    ) {
+                        if (hwnd != null && isValidWindow(hwnd)) {
+                            lastActiveWindow = hwnd
+                        }
                     }
                 }
-            }
 
-        hookHandle =
-            INSTANCE.SetWinEventHook(
-                User32.EVENT_SYSTEM_FOREGROUND,
-                User32.EVENT_SYSTEM_FOREGROUND,
-                null,
-                eventProc,
-                0,
-                0,
-                User32.WINEVENT_OUTOFCONTEXT,
-            )
+            hookHandle =
+                INSTANCE.SetWinEventHook(
+                    User32.EVENT_SYSTEM_FOREGROUND,
+                    User32.EVENT_SYSTEM_FOREGROUND,
+                    null,
+                    eventProc,
+                    0,
+                    0,
+                    User32.WINEVENT_OUTOFCONTEXT,
+                )
+
+            val msg = WinUser.MSG()
+            while (INSTANCE.GetMessage(msg, null, 0, 0) != -1) {
+                INSTANCE.TranslateMessage(msg)
+                INSTANCE.DispatchMessage(msg)
+            }
+        }
     }
 
     fun stop() {
