ab23f53157c7579ab582f29b5ba894a345489cec
Yiqun Zhang <guiyanakuang@gmail.com>
2025-12-09 17:10:14 +0800

    :lipstick: Implement native tray support for Windows and Linux

--- app/src/desktopMain/kotlin/com/crosspaste/ui/tray/NonMacTrayView.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/ui/tray/NonMacTrayView.kt
@@ -1,6 +1,7 @@
 package com.crosspaste.ui.tray
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.window.ApplicationScope
 import com.crosspaste.app.AppName
@@ -8,6 +9,7 @@ import com.crosspaste.app.DesktopAppWindowManager
 import com.crosspaste.app.ExitMode
 import com.crosspaste.app.WindowTrigger
 import com.crosspaste.i18n.GlobalCopywriter
+import com.crosspaste.platform.Platform
 import com.crosspaste.ui.LocalExitApplication
 import com.crosspaste.ui.base.MenuHelper
 import com.crosspaste.utils.GlobalCoroutineScope.mainCoroutineDispatcher
@@ -20,7 +22,11 @@ fun ApplicationScope.NonMacTrayView(windowIcon: Painter) {
     val appWindowManager = koinInject<DesktopAppWindowManager>()
     val copywriter = koinInject<GlobalCopywriter>()
     val menuHelper = koinInject<MenuHelper>()
+    val platform = koinInject<Platform>()
+
     val applicationExit = LocalExitApplication.current
+
+    val isLinux = remember { platform.isLinux() }
 
     Tray(
         icon = windowIcon,
@@ -32,6 +38,17 @@ fun ApplicationScope.NonMacTrayView(windowIcon: Painter) {
             }
         },
     ) {
+        if (isLinux) {
+            Item(label = copywriter.getText("open_search_window")) {
+                mainCoroutineDispatcher.launch {
+                    appWindowManager.saveCurrentActiveAppInfo()
+                    appWindowManager.showMainWindow(WindowTrigger.MENU)
+                }
+            }
+
+            Divider()
+        }
+
         menuHelper.menuItems.forEach { item ->
             Item(label = item.title(copywriter)) {
                 item.action()
