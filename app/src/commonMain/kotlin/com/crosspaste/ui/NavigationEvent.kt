1960a3fc4a7aa527fb8011ce27169ce5ad6383a7
Yiqun Zhang <guiyanakuang@gmail.com>
2025-12-14 16:58:14 +0800

    :hammer: Refactor NavigationEvent from sealed class to interface pattern

--- app/src/commonMain/kotlin/com/crosspaste/ui/NavigationEvent.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/NavigationEvent.kt
@@ -1,13 +1,13 @@
 package com.crosspaste.ui
 
-sealed class NavigationEvent {
-    data class Navigate(
-        val route: Route,
-    ) : NavigationEvent()
+interface NavigationEvent
 
-    data class NavigateAndClearStack(
-        val route: Route,
-    ) : NavigationEvent()
+data class Navigate(
+    val route: Route,
+) : NavigationEvent
 
-    object NavigateUp : NavigationEvent()
-}
+data class NavigateAndClearStack(
+    val route: Route,
+) : NavigationEvent
+
+object NavigateUp : NavigationEvent
