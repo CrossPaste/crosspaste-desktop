07177928499d4e09bc0ca42805db5f8140be09c0
Yiqun Zhang <guiyanakuang@gmail.com>
2025-12-22 22:06:02 +0800

    :bug: Fix search window position flickering when opened

--- app/src/desktopTest/kotlin/com/crosspaste/app/AppWindowManagerTest.kt
+++ app/src/desktopTest/kotlin/com/crosspaste/app/AppWindowManagerTest.kt
@@ -20,7 +20,7 @@ class AppWindowManagerTest {
 
         val mockDesktopAppSize =
             spyk(DesktopAppSize(platform)) {
-                every { getSearchWindowState(false) } returns WindowState()
+                every { getSearchWindowState(any()) } returns WindowState()
             }
 
         val testAppWindowManager =
