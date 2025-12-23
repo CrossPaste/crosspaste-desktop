7f0ece7c3f3d28ff8bb5203e30d03054226cdbeb
Yiqun Zhang <guiyanakuang@gmail.com>
2025-12-23 21:11:18 +0800

    :spakles: Add support for F13-F20 function keys

--- app/src/desktopMain/kotlin/com/crosspaste/listener/WindowsKeyboardKeys.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/listener/WindowsKeyboardKeys.kt
@@ -115,6 +115,30 @@ object WindowsKeyboardKeys : KeyboardKeys {
     override val F12: KeyboardKeyDefine =
         KeyboardKeyDefine("F12", NativeKeyEvent.VC_F12, 0x7B) { it.keyCode == NativeKeyEvent.VC_F12 }
 
+    override val F13: KeyboardKeyDefine =
+        KeyboardKeyDefine("F13", NativeKeyEvent.VC_F13, 0x7C) { it.keyCode == NativeKeyEvent.VC_F13 }
+
+    override val F14: KeyboardKeyDefine =
+        KeyboardKeyDefine("F14", NativeKeyEvent.VC_F14, 0x7D) { it.keyCode == NativeKeyEvent.VC_F14 }
+
+    override val F15: KeyboardKeyDefine =
+        KeyboardKeyDefine("F15", NativeKeyEvent.VC_F15, 0x7E) { it.keyCode == NativeKeyEvent.VC_F15 }
+
+    override val F16: KeyboardKeyDefine =
+        KeyboardKeyDefine("F16", NativeKeyEvent.VC_F16, 0x7F) { it.keyCode == NativeKeyEvent.VC_F16 }
+
+    override val F17: KeyboardKeyDefine =
+        KeyboardKeyDefine("F17", NativeKeyEvent.VC_F17, 0x80) { it.keyCode == NativeKeyEvent.VC_F17 }
+
+    override val F18: KeyboardKeyDefine =
+        KeyboardKeyDefine("F18", NativeKeyEvent.VC_F18, 0x81) { it.keyCode == NativeKeyEvent.VC_F18 }
+
+    override val F19: KeyboardKeyDefine =
+        KeyboardKeyDefine("F19", NativeKeyEvent.VC_F19, 0x82) { it.keyCode == NativeKeyEvent.VC_F19 }
+
+    override val F20: KeyboardKeyDefine =
+        KeyboardKeyDefine("F20", NativeKeyEvent.VC_F20, 0x83) { it.keyCode == NativeKeyEvent.VC_F20 }
+
     override val _1: KeyboardKeyDefine =
         KeyboardKeyDefine("1", NativeKeyEvent.VC_1, 0x31) { it.keyCode == NativeKeyEvent.VC_1 }
 
