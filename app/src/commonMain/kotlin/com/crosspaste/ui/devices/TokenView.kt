7db0ee33585a38f5aefb1b7be4130badd6c26d2a
Yiqun Zhang <guiyanakuang@gmail.com>
2025-12-29 14:46:21 +0800

    :lipstick: Refactor Token view to comply with Material 3 principles

--- app/src/commonMain/kotlin/com/crosspaste/ui/devices/TokenView.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/devices/TokenView.kt
@@ -1,33 +1,32 @@
 package com.crosspaste.ui.devices
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
@@ -35,28 +34,22 @@ import com.crosspaste.app.AppTokenApi
 import com.crosspaste.i18n.GlobalCopywriter
 import com.crosspaste.ui.LocalAppSizeValueState
 import com.crosspaste.ui.base.close
-import com.crosspaste.ui.theme.AppUIColors
-import com.crosspaste.ui.theme.AppUIFont.generalTitleTextStyle
-import com.crosspaste.ui.theme.AppUIFont.tokenTextStyle
 import com.crosspaste.ui.theme.AppUISize.medium
-import com.crosspaste.ui.theme.AppUISize.mediumRoundedCornerShape
 import com.crosspaste.ui.theme.AppUISize.small
 import com.crosspaste.ui.theme.AppUISize.small2X
-import com.crosspaste.ui.theme.AppUISize.small3X
 import com.crosspaste.ui.theme.AppUISize.tiny
-import com.crosspaste.ui.theme.AppUISize.tiny3X
-import com.crosspaste.ui.theme.AppUISize.tiny3XRoundedCornerShape
-import com.crosspaste.ui.theme.AppUISize.tiny4XRoundedCornerShape
+import com.crosspaste.ui.theme.AppUISize.tiny2X
 import com.crosspaste.ui.theme.AppUISize.tiny5X
+import com.crosspaste.ui.theme.AppUISize.xxxLarge
+import com.crosspaste.ui.theme.AppUISize.xxxxLarge
+import com.crosspaste.ui.theme.AppUISize.zero
 import org.koin.compose.koinInject
 
 @Composable
 fun TokenView(intOffset: IntOffset) {
     val appTokenApi = koinInject<AppTokenApi>()
     val copywriter = koinInject<GlobalCopywriter>()
-
     val appSizeValue = LocalAppSizeValueState.current
-
     val showToken by appTokenApi.showToken.collectAsState()
 
     if (showToken) {
@@ -65,80 +58,52 @@ fun TokenView(intOffset: IntOffset) {
             offset = intOffset,
             properties = PopupProperties(clippingEnabled = false),
         ) {
-            Box(
+            Surface(
                 modifier =
                     Modifier
                         .width(appSizeValue.tokenViewWidth)
-                        .wrapContentHeight()
-                        .background(Color.Transparent)
-                        .shadow(small),
+                        .wrapContentHeight(),
+                shape = MaterialTheme.shapes.large,
+                color = MaterialTheme.colorScheme.surfaceContainer,
+                tonalElevation = small,
+                shadowElevation = small,
             ) {
                 Column(
                     modifier =
                         Modifier
-                            .wrapContentSize()
-                            .clip(tiny3XRoundedCornerShape)
-                            .align(Alignment.Center)
-                            .background(AppUIColors.topBackground),
+                            .fillMaxWidth()
+                            .padding(bottom = small2X),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     Box(
                         modifier =
                             Modifier
-                                .align(Alignment.CenterHorizontally)
-                                .wrapContentHeight()
-                                .padding(horizontal = tiny)
-                                .padding(top = tiny),
+                                .fillMaxWidth()
+                                .padding(top = small, start = small2X, end = tiny),
                         contentAlignment = Alignment.Center,
                     ) {
                         Text(
                             text = copywriter.getText("token"),
-                            color =
-                                MaterialTheme.colorScheme.contentColorFor(
-                                    AppUIColors.topBackground,
-                                ),
-                            style = generalTitleTextStyle,
+                            style = MaterialTheme.typography.titleLarge,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
                         )
 
-                        Box(
-                            modifier =
-                                Modifier
-                                    .fillMaxWidth()
-                                    .wrapContentHeight()
-                                    .padding(end = tiny3X),
-                            contentAlignment = Alignment.CenterEnd,
+                        // Close Button aligned to the end
+                        IconButton(
+                            onClick = { appTokenApi.stopRefresh(hideToken = true) },
+                            modifier = Modifier.align(Alignment.CenterEnd),
                         ) {
-                            Box(
-                                modifier =
-                                    Modifier
-                                        .size(medium * 2)
-                                        .clip(mediumRoundedCornerShape)
-                                        .clickable {
-                                            appTokenApi.stopRefresh(hideToken = true)
-                                        },
-                                contentAlignment = Alignment.Center,
-                            ) {
-                                Icon(
-                                    painter = close(),
-                                    contentDescription = "Close",
-                                    modifier = Modifier.size(medium),
-                                    tint =
-                                        MaterialTheme.colorScheme.contentColorFor(
-                                            AppUIColors.topBackground,
-                                        ),
-                                )
-                            }
+                            Icon(
+                                painter = close(),
+                                contentDescription = "Close",
+                                modifier = Modifier.size(medium),
+                                tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                            )
                         }
                     }
-                    Row(
-                        modifier =
-                            Modifier
-                                .padding(horizontal = tiny3X, vertical = small2X)
-                                .wrapContentSize(),
-                        horizontalArrangement = Arrangement.Center,
-                    ) {
-                        OTPCodeBox()
-                    }
+
+                    // Token Display Section
+                    OTPCodeBox()
                 }
             }
         }
@@ -155,42 +120,65 @@ private fun OTPCodeBox() {
         modifier =
             Modifier
                 .fillMaxWidth()
-                .wrapContentHeight(),
+                .padding(horizontal = small2X),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Row(horizontalArrangement = Arrangement.spacedBy(small2X)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(small),
+            modifier = Modifier.padding(vertical = small2X),
+        ) {
             token.forEach { char ->
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier =
-                        Modifier
-                            .background(MaterialTheme.colorScheme.surfaceVariant, tiny3XRoundedCornerShape)
-                            .border(tiny5X, AppUIColors.importantColor, tiny3XRoundedCornerShape)
-                            .padding(vertical = tiny, horizontal = small2X),
-                ) {
-                    Text(
-                        text = char.toString(),
-                        color = AppUIColors.importantColor,
-                        style = tokenTextStyle,
-                    )
-                }
+                TokenDisplayBox(char = char.toString())
             }
         }
-        Spacer(modifier = Modifier.height(small3X))
-        Row(
+
+        Spacer(modifier = Modifier.height(small))
+
+        LinearProgressIndicator(
+            progress = { progress },
             modifier =
                 Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(horizontal = small3X),
+                    .fillMaxWidth(0.9f)
+                    .height(tiny2X)
+                    .clip(MaterialTheme.shapes.extraLarge),
+            color = MaterialTheme.colorScheme.primary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
+}
+
+@Composable
+private fun TokenDisplayBox(
+    char: String,
+    modifier: Modifier = Modifier,
+) {
+    val shape = MaterialTheme.shapes.medium
+
+    Surface(
+        modifier =
+            modifier
+                .size(width = xxxLarge, height = xxxxLarge)
+                .border(
+                    width = tiny5X,
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
+                    shape = shape,
+                ),
+        shape = shape,
+        color = MaterialTheme.colorScheme.surfaceContainerHighest,
+        tonalElevation = zero,
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize(),
         ) {
-            LinearProgressIndicator(
-                modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .height(tiny3X)
-                        .clip(tiny4XRoundedCornerShape),
-                progress = { progress },
+            Text(
+                text = char,
+                style =
+                    MaterialTheme.typography.headlineMedium.copy(
+                        color = MaterialTheme.colorScheme.primary,
+                        textAlign = TextAlign.Center,
+                        fontFamily = FontFamily.Monospace,
+                    ),
             )
         }
     }
