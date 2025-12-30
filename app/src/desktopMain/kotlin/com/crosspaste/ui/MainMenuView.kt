2518d615400c06ec948b3603c50dc2cf6c1fab48
Yiqun Zhang <guiyanakuang@gmail.com>
2025-12-30 18:58:26 +0800

    :lipstick: Refactor MainMenuView to fully comply with Material 3 design principles

--- app/src/desktopMain/kotlin/com/crosspaste/ui/MainMenuView.kt
+++ app/src/desktopMain/kotlin/com/crosspaste/ui/MainMenuView.kt
@@ -2,32 +2,27 @@ package com.crosspaste.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.crosspaste.app.AppUpdateService
 import com.crosspaste.app.DesktopAppLaunch
@@ -35,13 +30,11 @@ import com.crosspaste.app.ExitMode
 import com.crosspaste.config.DesktopConfigManager
 import com.crosspaste.i18n.GlobalCopywriter
 import com.crosspaste.ui.base.TutorialButton
-import com.crosspaste.ui.theme.AppUIColors
-import com.crosspaste.ui.theme.AppUISize
+import com.crosspaste.ui.theme.AppUISize.large2X
 import com.crosspaste.ui.theme.AppUISize.medium
-import com.crosspaste.ui.theme.AppUISize.small3X
+import com.crosspaste.ui.theme.AppUISize.small2X
 import com.crosspaste.ui.theme.AppUISize.tiny
-import com.crosspaste.ui.theme.AppUISize.tiny3X
-import com.crosspaste.ui.theme.AppUISize.tinyRoundedCornerShape
+import com.crosspaste.ui.theme.AppUISize.tiny4X
 import org.koin.compose.koinInject
 
 @Composable
@@ -51,144 +44,120 @@ fun MainMenuView() {
     val configManager = koinInject<DesktopConfigManager>()
     val navigateManage = koinInject<NavigationManager>()
 
-    val prevMenuList by remember {
-        mutableStateOf(
+    val prevMenuList =
+        remember {
             listOf(
                 MainMenuItem("devices", Devices),
                 MainMenuItem("scan", QrCode),
                 MainMenuItem("settings", Settings),
                 MainMenuItem("extension", Extension),
-            ),
-        )
-    }
+            )
+        }
 
-    val nextMenuList by remember {
-        mutableStateOf(
+    val nextMenuList =
+        remember {
             listOf(
                 MainMenuItem("import", Import),
                 MainMenuItem("export", Export),
                 MainMenuItem("shortcut_keys", ShortcutKeys),
                 MainMenuItem("recommend", Recommend),
                 MainMenuItem("about", About),
-            ),
-        )
-    }
+            )
+        }
 
     val config by configManager.config.collectAsState()
-
     val firstLaunchCompleted by appLaunch.firstLaunchCompleted.collectAsState()
-
     val navController = LocalNavHostController.current
-
     val backStackEntry by navController.currentBackStackEntryAsState()
-
     val rootRouteName = backStackEntry?.let { getRootRouteName(it.destination) }
-
-    val selectedIndex by remember(rootRouteName) {
-        mutableStateOf(
-            (prevMenuList + nextMenuList).indexOfFirst {
-                it.route.name == rootRouteName
-            },
-        )
-    }
-
     val exitApplication = LocalExitApplication.current
 
     Column(
         modifier =
             Modifier
                 .fillMaxSize()
-                .padding(vertical = medium),
+                .padding(bottom = medium),
     ) {
-        prevMenuList.forEachIndexed { index, item ->
-            MainMenuItemView(item.title, background(index == selectedIndex)) {
-                navigateManage.navigateAndClearStack(item.route)
-            }
+        prevMenuList.forEach { item ->
+            MainMenuItemView(
+                title = item.title,
+                selected = rootRouteName == item.route.name,
+                onClick = { navigateManage.navigateAndClearStack(item.route) },
+            )
         }
 
         Spacer(Modifier.weight(1f))
 
         if (firstLaunchCompleted && config.showTutorial) {
-            Row(
-                modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(start = small3X, bottom = tiny),
-            ) {
+            Box(modifier = Modifier.padding(horizontal = medium, vertical = tiny)) {
                 TutorialButton()
             }
         }
 
-        nextMenuList.forEachIndexed { index, item ->
-            MainMenuItemView(item.title, background(index == selectedIndex - prevMenuList.size)) {
-                navigateManage.navigateAndClearStack(item.route)
-            }
+        nextMenuList.forEach { item ->
+            MainMenuItemView(
+                title = item.title,
+                selected = rootRouteName == item.route.name,
+                onClick = { navigateManage.navigateAndClearStack(item.route) },
+            )
         }
 
-        MainMenuItemView("check_for_updates", AppUIColors.generalBackground) {
+        MainMenuItemView("check_for_updates", false) {
             appUpdateService.tryTriggerUpdate()
         }
 
-        MainMenuItemView("quit", AppUIColors.generalBackground) {
+        MainMenuItemView("quit", false) {
             exitApplication(ExitMode.EXIT)
         }
     }
 }
 
 @Composable
-private fun background(selected: Boolean): Color =
-    if (selected) {
-        MaterialTheme.colorScheme.primaryContainer
-    } else {
-        AppUIColors.generalBackground
-    }
-
-@Composable
 fun MainMenuItemView(
     title: String,
-    background: Color,
+    selected: Boolean,
     onClick: () -> Unit,
 ) {
     val copywriter = koinInject<GlobalCopywriter>()
+
+    val containerColor =
+        if (selected) {
+            MaterialTheme.colorScheme.primaryContainer
+        } else {
+            Color.Transparent
+        }
+
+    val contentColor =
+        if (selected) {
+            MaterialTheme.colorScheme.onPrimaryContainer
+        } else {
+            MaterialTheme.colorScheme.onSurfaceVariant
+        }
 
     Box(
         modifier =
             Modifier
                 .fillMaxWidth()
-                .height(AppUISize.xxxLarge),
-        contentAlignment = Alignment.Center,
+                .height(large2X * 2)
+                .padding(horizontal = tiny, vertical = tiny4X)
+                .clip(RoundedCornerShape(tiny))
+                .background(containerColor)
+                .clickable(onClick = onClick)
+                .padding(horizontal = small2X),
+        contentAlignment = Alignment.CenterStart,
     ) {
-        Row(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding(horizontal = small3X)
-                    .clip(tinyRoundedCornerShape)
-                    .background(background)
-                    .clickable(onClick = onClick),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Row(
-                modifier = Modifier.padding(start = small3X, end = tiny3X),
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = copywriter.getText(title),
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    color = MaterialTheme.colorScheme.contentColorFor(background),
-                    textAlign = TextAlign.Start,
-                    style =
-                        MaterialTheme.typography.titleSmall.copy(
-                            fontWeight = FontWeight.Light,
-                            lineHeight = TextUnit.Unspecified,
-                        ),
-                )
-            }
-        }
+        Text(
+            text = copywriter.getText(title),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = contentColor,
+            style =
+                MaterialTheme.typography.bodyMedium.copy(
+                    fontWeight = if (selected) FontWeight.SemiBold else FontWeight.Normal,
+                    // Adjusting line height and letter spacing for desktop clarity
+                    letterSpacing = 0.2.sp,
+                ),
+        )
     }
 }
 
