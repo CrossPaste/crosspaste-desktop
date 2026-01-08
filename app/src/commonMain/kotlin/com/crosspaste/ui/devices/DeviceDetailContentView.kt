c669764baf2a6e2d7a69bc5cda834df15eb514b9
Yiqun Zhang <guiyanakuang@gmail.com>
2026-01-08 14:15:56 +0800

    :hammer: Refactor device detail screens to use ScreenLayout (#3594)

--- app/src/commonMain/kotlin/com/crosspaste/ui/devices/DeviceDetailContentView.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/devices/DeviceDetailContentView.kt
@@ -1,42 +1,28 @@
 package com.crosspaste.ui.devices
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.crosspaste.ui.theme.AppUIColors
-import com.crosspaste.ui.theme.AppUISize.medium
 import com.crosspaste.ui.theme.AppUISize.xLarge
 
 @Composable
 fun DeviceScope.DeviceDetailContentView() {
-    Box(
+    val scrollState = rememberScrollState()
+
+    Column(
         modifier =
             Modifier
                 .fillMaxSize()
-                .background(AppUIColors.appBackground)
-                .padding(horizontal = medium)
-                .padding(bottom = medium),
+                .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.spacedBy(xLarge),
     ) {
-        val scrollState = rememberScrollState()
-
-        Column(
-            modifier =
-                Modifier
-                    .fillMaxSize()
-                    .verticalScroll(scrollState),
-            verticalArrangement = Arrangement.spacedBy(xLarge),
-        ) {
-            DeviceDetailHeaderView()
-            IncompatibleSection()
-            SyncControlSection()
-            DeviceInfoSection()
-        }
+        DeviceDetailHeaderView()
+        IncompatibleSection()
+        SyncControlSection()
+        DeviceInfoSection()
     }
 }
--- app/src/commonMain/kotlin/com/crosspaste/ui/devices/NearbyDeviceDetailContentView.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/devices/NearbyDeviceDetailContentView.kt
@@ -2,38 +2,26 @@ package com.crosspaste.ui.devices
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import com.crosspaste.ui.theme.AppUIColors
-import com.crosspaste.ui.theme.AppUISize.medium
 import com.crosspaste.ui.theme.AppUISize.tinyRoundedCornerShape
 import com.crosspaste.ui.theme.AppUISize.xLarge
 
 @Composable
 fun SyncScope.NearbyDeviceDetailContentView() {
-    Box(
+    Column(
         modifier =
             Modifier
                 .fillMaxSize()
                 .background(AppUIColors.appBackground)
-                .padding(horizontal = medium)
-                .padding(bottom = medium),
+                .clip(tinyRoundedCornerShape),
+        verticalArrangement = Arrangement.spacedBy(xLarge),
     ) {
-        Column(
-            modifier =
-                Modifier
-                    .fillMaxSize()
-                    .background(AppUIColors.appBackground)
-                    .clip(tinyRoundedCornerShape),
-            verticalArrangement = Arrangement.spacedBy(xLarge),
-        ) {
-            NearbyDeviceDetailHeaderView()
-            NearbyDeviceInfoSection()
-        }
+        NearbyDeviceDetailHeaderView()
+        NearbyDeviceInfoSection()
     }
 }
