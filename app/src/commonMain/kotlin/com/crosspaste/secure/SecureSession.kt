f29039194b08c34883bfb9b6b9e992fb3548daea
Yiqun Zhang <guiyanakuang@gmail.com>
2026-02-16 11:25:10 +0800

    :bug: Fix SecureStore double-check locking and session lifecycle bugs

--- app/src/commonMain/kotlin/com/crosspaste/secure/SecureSession.kt
+++ app/src/commonMain/kotlin/com/crosspaste/secure/SecureSession.kt
@@ -4,5 +4,5 @@ import kotlinx.coroutines.sync.Mutex
 
 data class SecureSession(
     val mutex: Mutex = Mutex(),
-    var processor: SecureMessageProcessor? = null,
+    @Volatile var processor: SecureMessageProcessor? = null,
 )
