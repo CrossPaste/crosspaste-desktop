d12070b17c33a7f76c3e93f9b72fe58b8c8f50bd
Yiqun Zhang <guiyanakuang@gmail.com>
2026-02-17 21:45:14 +0800

    :art: Add missing @Volatile explicit imports (#3918)

--- app/src/commonMain/kotlin/com/crosspaste/secure/SecureSession.kt
+++ app/src/commonMain/kotlin/com/crosspaste/secure/SecureSession.kt
@@ -1,6 +1,7 @@
 package com.crosspaste.secure
 
 import kotlinx.coroutines.sync.Mutex
+import kotlin.concurrent.Volatile
 
 data class SecureSession(
     val mutex: Mutex = Mutex(),
--- app/src/commonMain/kotlin/com/crosspaste/ui/model/PasteSearchViewModel.kt
+++ app/src/commonMain/kotlin/com/crosspaste/ui/model/PasteSearchViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlin.concurrent.Volatile
 
 abstract class PasteSearchViewModel : ViewModel() {
 
