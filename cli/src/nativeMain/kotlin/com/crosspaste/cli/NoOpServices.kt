76937687adae68af1bb7d53bd14a5bf7ca255366
Yiqun Zhang <guiyanakuang@gmail.com>
2026-02-26 13:25:34 +0800

    :art: Remove unused NoOpTaskBuilder class

--- cli/src/nativeMain/kotlin/com/crosspaste/cli/NoOpServices.kt
+++ cli/src/nativeMain/kotlin/com/crosspaste/cli/NoOpServices.kt
@@ -1,6 +1,5 @@
 package com.crosspaste.cli
 
-import com.crosspaste.paste.PasteType
 import com.crosspaste.paste.SearchContentService
 import com.crosspaste.task.TaskBuilder
 import com.crosspaste.task.TaskSubmitter
@@ -21,39 +20,4 @@ class NoOpTaskSubmitter : TaskSubmitter {
     override suspend fun submit(block: suspend TaskBuilder.() -> Unit) {
         // CLI is read-only, no background tasks
     }
-}
-
-private class NoOpTaskBuilder : TaskBuilder {
-    override fun addDelayedDeletePasteTask(
-        id: Long,
-        delayMillis: Long,
-    ) = this
-
-    override fun addDeletePasteTasks(ids: List<Long>) = this
-
-    override fun addPullFileTask(
-        id: Long,
-        remotePasteDataId: Long,
-    ) = this
-
-    override fun addSyncTask(
-        id: Long,
-        appInstanceId: String,
-        fileSize: Long,
-    ) = this
-
-    override fun addRelaySyncTask(
-        id: Long,
-        appInstanceId: String,
-    ) = this
-
-    override fun addPullIconTask(
-        id: Long,
-        existIconFile: Boolean,
-    ) = this
-
-    override fun addRenderingTask(
-        id: Long,
-        pasteType: PasteType,
-    ) = this
 }
